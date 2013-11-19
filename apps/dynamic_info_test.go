799e13cde44b9127f9acce079b9b52e01219cff3
Alex Suraci & Tim Labeeuw <pair+alex+tim@pivotallabs.com>
2013-11-18 17:51:49 -0800

    fix flaky cf files test
    We were testing against an app with two instances. `gcf` only waits for
    one index to report RUNNING, but `gcf files` always checks index 0. So,
    if index 1 came up before 0, `gcf files` would reliably fail.
    
    There is a bug in `gcf` here: `gcf files` should grab files of only a
    running instance, potentially a random one, to fit the abstraction level
    of `gcf push` (specific instances don't matter).
    
    [finishes #60908262]

--- apps/dynamic_info_test.go
+++ apps/dynamic_info_test.go
@@ -14,22 +14,15 @@ var _ = Describe("A running application", func() {
 	BeforeEach(func() {
 		AppName = RandomName()
 
-		Expect(
-			Cf("push", AppName, "-p", doraPath, "-i", "2"),
-		).To(Say("Started"))
+		Expect(Cf("push", AppName, "-p", doraPath)).To(Say("Started"))
 	})
 
 	AfterEach(func() {
 		Expect(Cf("delete", AppName, "-f")).To(Say("OK"))
 	})
 
-	It("can be queried for state by instance", func() {
-		app := Cf("app", AppName)
-		Expect(app).To(Say("#0"))
-		Expect(app).To(Say("#1"))
-	})
-
 	It("can have its files inspected", func() {
+		// Currently cannot work with multiple instances since GCF always checks instance 0
 		Expect(Cf("files", AppName)).To(Say("app/"))
 		Expect(Cf("files", AppName, "app/")).To(Say("config.ru"))
 		Expect(Cf("files", AppName, "app/config.ru")).To(
@@ -42,5 +35,19 @@ var _ = Describe("A running application", func() {
 		Eventually(func() *cmdtest.Session {
 			return Cf("events", AppName)
 		}, 10).Should(Say("exited"))
+	})
+	
+	Context("with multiple instances", func() {
+		BeforeEach(func() {
+			Expect(
+				Cf("scale", AppName, "-i", "2"),
+			).To(Say("OK"))
+		})
+
+		It("can be queried for state by instance", func() {
+			app := Cf("app", AppName)
+			Expect(app).To(Say("#0"))
+			Expect(app).To(Say("#1"))
+		})
 	})
 })
