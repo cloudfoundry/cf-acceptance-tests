2a5f1139f44aa9b84109bb91399e327b06bbd471
Dave Liebreich <dliebreich@pivotallabs.com>
2014-01-20 16:38:44 -0800

    add tagged loglines

--- assets/dora/log_utils.rb
+++ assets/dora/log_utils.rb
@@ -1,9 +1,20 @@
 class LogUtils < Sinatra::Base
+
   get "/loglines/:linecount" do
-    params[:linecount].to_i.times do |i|
-      puts "#{Time.now.strftime("%FT%T.%N%:z")} line #{i}"
-    end
+    produce_log_output(params[:linecount])
     "logged #{params[:linecount]} line to stdout"
   end
 
+  get "/loglines/:linecount/:tag" do
+    produce_log_output(params[:linecount], params[:tag])
+    "logged #{params[:linecount]} line with tag #{params[:tag]} to stdout"
+  end
+
+  private
+  def produce_log_output(linecount, tag="")
+    linecount.to_i.times do |i|
+      puts "#{Time.now.strftime("%FT%T.%N%:z")} line #{i} #{tag}"
+      $stdout.flush
+    end
+  end
 end
--- assets/dora/spec/logutil_spec.rb
+++ assets/dora/spec/logutil_spec.rb
@@ -2,11 +2,14 @@ require "spec_helper"
 
 describe LogUtils do
   describe "GET /loglines" do
-    let(:id) { "b4ffb1a7b677447296f9ff7eea535c43" }
-
     it "should output one line" do
       get "/loglines/1"
       expect(last_response.body).to eq "logged 1 line to stdout"
+    end
+
+    it "should annotate lines" do
+      get "/loglines/1/unique_tag"
+      expect(last_response.body).to eq "logged 1 line with tag unique_tag to stdout"
     end
   end
 end
