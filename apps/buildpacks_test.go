c1ac0863c5aea504528a73dc2f12902d3372fb24
Matthew Sykes <matthew.sykes@gmail.com>
2014-07-02 13:51:31 -0400

    Increase php push timeout to 8 minutes
    6 minutes just wasn't enough; let's try 8...
    
    Signed-off-by: Joseph Palermo <jpalermo@pivotallabs.com>

--- apps/buildpacks_test.go
+++ apps/buildpacks_test.go
@@ -57,7 +57,7 @@ var _ = Describe("Buildpacks", func() {
 
 	Describe("php", func() {
 		// This test requires more time during push, because the php buildpack is slower than your average bear
-		var phpPushTimeout = CF_PUSH_TIMEOUT + 4*time.Minute
+		var phpPushTimeout = CF_PUSH_TIMEOUT + 6*time.Minute
 
 		It("makes the app reachable via its bound route", func() {
 			Expect(cf.Cf("push", appName, "-p", helpers.NewAssets().Php).Wait(phpPushTimeout)).To(Exit(0))
