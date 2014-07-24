fdf779e6a97445c78a262ee718c93e97f4c226f1
Julian Friedman <julz.friedman@uk.ibm.com>
2014-07-24 13:06:22 +0100

    Give loggregator time to start streaming
    Signed-off-by: Matthew Sykes <matthew.sykes@gmail.com>

--- apps/admin_buildpack_lifecycle_test.go
+++ apps/admin_buildpack_lifecycle_test.go
@@ -51,6 +51,7 @@ var _ = Describe("Admin Buildpacks", func() {
 					Name: "bin/compile",
 					Body: `#!/usr/bin/env bash
 
+sleep 1 # give loggregator time to start streaming the logs
 
 echo "Staging with Simple Buildpack"
 
