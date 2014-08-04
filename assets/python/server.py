e58ae1602f164e5906f192a2fc559abc48c355cd
Rasheed Abdul-Aziz <pair+squeedee@pivotallabs.com>
2014-08-04 14:08:02 -0400

    Python asset should not have dependencies
    https://www.pivotaltracker.com/story/show/75931076

--- assets/python/server.py
+++ assets/python/server.py
@@ -1,9 +1,19 @@
-from flask import Flask
-app = Flask(__name__)
+import sys
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
-@app.route("/")
-def hello():
-    return "python, world"
+port = int(sys.argv[1])
 
-if __name__ == "__main__":
-    app.run()
+class requestHandler(BaseHTTPRequestHandler):
+    def do_GET(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        s.wfile.write("<html>")
+        s.wfile.write("<head><title>Python app.</title></head>")
+        s.wfile.write("<body><p>python, world</p></body>")
+        s.wfile.write("</html>")
+        return
+
+web_server = HTTPServer(('0.0.0.0', port), requestHandler)
+print 'Started on port', port
+web_server.serve_forever()
