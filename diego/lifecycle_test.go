22bfc8ccad309abb0181531aea684f9039a35a1f
Stew Nickolas <nickolas@us.ibm.com>
2014-08-05 15:27:51 -0700

    Add test for switching DEA app to diego
    Signed-off-by: Max Brunsfeld <mbrunsfeld@pivotallabs.com>

--- diego/lifecycle_test.go
+++ diego/lifecycle_test.go
@@ -94,4 +94,21 @@ var _ = Describe("Application Lifecycle", func() {
 
 		describeLifeCycle()
 	})
+
+	Describe("An existing DEA-based app being migrated to Diego", func() {
+		BeforeEach(func() {
+			appName = generator.RandomName()
+			Eventually(cf.Cf("push", appName, "-p", helpers.NewAssets().Dora), CF_PUSH_TIMEOUT).Should(Exit(0))
+			Eventually(cf.Cf("stop", appName), DEFAULT_TIMEOUT).Should(Exit(0))
+			Eventually(helpers.CurlingAppRoot(appName)).Should(ContainSubstring("404"))
+			Eventually(cf.Cf("set-env", appName, "CF_DIEGO_RUN_BETA", "true"), DEFAULT_TIMEOUT).Should(Exit(0)) // CF_DIEGO_RUN_BETA also implies CF_DIEGO_BETA in CC
+			Eventually(cf.Cf("start", appName), CF_PUSH_TIMEOUT).Should(Exit(0))
+		})
+
+		AfterEach(func() {
+			Eventually(cf.Cf("delete", appName, "-f"), DEFAULT_TIMEOUT).Should(Exit(0))
+		})
+
+		describeLifeCycle()
+	})
 })
