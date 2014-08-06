1a219da757f6193496063c3694c169a29db443d8
Max Brunsfeld <mbrunsfeld@pivotallabs.com>
2014-08-06 12:32:16 -0700

    Reduce odds of timeout error in lifecycle test
    - Consolidate tests that stop and start app
    - Make sure only one buildpack is downloaded
    
    Signed-off-by: Simon Leung <leungs@us.ibm.com>

--- diego/lifecycle_test.go
+++ diego/lifecycle_test.go
@@ -14,30 +14,25 @@ import (
 var _ = Describe("Application Lifecycle", func() {
 	var appName string
 
+	BeforeEach(func() {
+		appName = generator.RandomName()
+		Eventually(cf.Cf("push", appName, "-p", helpers.NewAssets().Dora, "--no-start", "-b", "ruby_buildpack"), CF_PUSH_TIMEOUT).Should(Exit(0))
+	})
+
+	AfterEach(func() {
+		Eventually(cf.Cf("delete", appName, "-f"), DEFAULT_TIMEOUT).Should(Exit(0))
+	})
+
 	describeLifeCycle := func() {
-		Describe("pushing", func() {
-			It("makes the app reachable via its bound route", func() {
+		Describe("stopping and starting the app", func() {
+			It("makes the app unreachable while it is stopped", func() {
 				Eventually(helpers.CurlingAppRoot(appName)).Should(ContainSubstring("Hi, I'm Dora!"))
-			})
-		})
 
-		Describe("stopping", func() {
-			BeforeEach(func() {
 				Eventually(cf.Cf("stop", appName), DEFAULT_TIMEOUT).Should(Exit(0))
-			})
-
-			It("makes the app unreachable", func() {
 				Eventually(helpers.CurlingAppRoot(appName)).Should(ContainSubstring("404"))
-			})
 
-			Describe("and then starting", func() {
-				BeforeEach(func() {
-					Eventually(cf.Cf("start", appName), DEFAULT_TIMEOUT).Should(Exit(0))
-				})
-
-				It("makes the app reachable again", func() {
-					Eventually(helpers.CurlingAppRoot(appName)).Should(ContainSubstring("Hi, I'm Dora!"))
-				})
+				Eventually(cf.Cf("start", appName), DEFAULT_TIMEOUT).Should(Exit(0))
+				Eventually(helpers.CurlingAppRoot(appName)).Should(ContainSubstring("Hi, I'm Dora!"))
 			})
 		})
 
@@ -65,15 +60,8 @@ var _ = Describe("Application Lifecycle", func() {
 
 	Describe("An app staged with Diego and running on a DEA", func() {
 		BeforeEach(func() {
-			appName = generator.RandomName()
-
-			Eventually(cf.Cf("push", appName, "-p", helpers.NewAssets().Dora, "--no-start"), CF_PUSH_TIMEOUT).Should(Exit(0))
 			Eventually(cf.Cf("set-env", appName, "CF_DIEGO_BETA", "true"), DEFAULT_TIMEOUT).Should(Exit(0))
 			Eventually(cf.Cf("start", appName), CF_PUSH_TIMEOUT).Should(Exit(0))
-		})
-
-		AfterEach(func() {
-			Eventually(cf.Cf("delete", appName, "-f"), DEFAULT_TIMEOUT).Should(Exit(0))
 		})
 
 		describeLifeCycle()
@@ -81,15 +69,8 @@ var _ = Describe("Application Lifecycle", func() {
 
 	Describe("An app both staged and run with Diego", func() {
 		BeforeEach(func() {
-			appName = generator.RandomName()
-
-			Eventually(cf.Cf("push", appName, "-p", helpers.NewAssets().Dora, "--no-start"), CF_PUSH_TIMEOUT).Should(Exit(0))
 			Eventually(cf.Cf("set-env", appName, "CF_DIEGO_RUN_BETA", "true"), DEFAULT_TIMEOUT).Should(Exit(0)) // CF_DIEGO_RUN_BETA also implies CF_DIEGO_BETA in CC
 			Eventually(cf.Cf("start", appName), CF_PUSH_TIMEOUT).Should(Exit(0))
-		})
-
-		AfterEach(func() {
-			Eventually(cf.Cf("delete", appName, "-f"), DEFAULT_TIMEOUT).Should(Exit(0))
 		})
 
 		describeLifeCycle()
@@ -97,16 +78,13 @@ var _ = Describe("Application Lifecycle", func() {
 
 	Describe("An existing DEA-based app being migrated to Diego", func() {
 		BeforeEach(func() {
-			appName = generator.RandomName()
-			Eventually(cf.Cf("push", appName, "-p", helpers.NewAssets().Dora), CF_PUSH_TIMEOUT).Should(Exit(0))
+			Eventually(cf.Cf("start", appName), CF_PUSH_TIMEOUT).Should(Exit(0))
+
 			Eventually(cf.Cf("stop", appName), DEFAULT_TIMEOUT).Should(Exit(0))
 			Eventually(helpers.CurlingAppRoot(appName)).Should(ContainSubstring("404"))
+
 			Eventually(cf.Cf("set-env", appName, "CF_DIEGO_RUN_BETA", "true"), DEFAULT_TIMEOUT).Should(Exit(0)) // CF_DIEGO_RUN_BETA also implies CF_DIEGO_BETA in CC
 			Eventually(cf.Cf("start", appName), CF_PUSH_TIMEOUT).Should(Exit(0))
-		})
-
-		AfterEach(func() {
-			Eventually(cf.Cf("delete", appName, "-f"), DEFAULT_TIMEOUT).Should(Exit(0))
 		})
 
 		describeLifeCycle()
