8ed9470f6780ff2a33b70ad79528cef7be5739be
Peter Parente <pparent@us.ibm.com>
2014-08-26 16:40:40 -0400

    Add CAT for docker env vars
    [#75752312]
    
    Signed-off-by: Stewart Nickolas <nickolas@us.ibm.com>

--- diego/lifecycle_docker_test.go
+++ diego/lifecycle_docker_test.go
@@ -62,6 +62,25 @@ var _ = Describe("Docker Application Lifecycle", func() {
 		Eventually(cf.Cf("delete", appName, "-f"), DEFAULT_TIMEOUT).Should(Exit(0))
 	})
 
+	Describe("running the app", func() {
+		It("merges the warden and docker environment variables", func() {
+			env_json := helpers.CurlApp(appName, "/env")
+			var env_vars map[string]string
+			json.Unmarshal([]byte(env_json), &env_vars)
+
+			// warden set values should win
+			Ω(env_vars).Should(HaveKey("HOME"))
+			Ω(env_vars).ShouldNot(HaveKeyWithValue("HOME", "/home/some_docker_user"))
+			Ω(env_vars).Should(HaveKey("VCAP_APPLICATION"))
+			Ω(env_vars).ShouldNot(HaveKeyWithValue("VCAP_APPLICATION", "{}"))
+			// docker image values should remain
+			Ω(env_vars).Should(HaveKeyWithValue("SOME_VAR", "some_docker_value"))
+			Ω(env_vars).Should(HaveKeyWithValue("BAD_QUOTE", "'"))
+			Ω(env_vars).Should(HaveKeyWithValue("BAD_SHELL", "$1"))
+
+		})
+	})
+
 	Describe("stopping an app", func() {
 		It("makes the app unreachable while it is stopped", func() {
 			Eventually(helpers.CurlingAppRoot(appName)).Should(Equal("0"))
