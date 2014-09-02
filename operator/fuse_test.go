8298114f725857cb77690c02a0db375b995383e9
Matthew Sykes <matthew.sykes@gmail.com>
2014-09-02 09:53:23 -0700

    Add fuse test to operator bucket.
    Signed-off-by: Zach Robinson <zrobinson@pivotal.io>

---
+++ operator/fuse_test.go
@@ -0,0 +1,31 @@
+package operator
+
+import (
+	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
+	"github.com/cloudfoundry/cf-acceptance-tests/helpers"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("FUSE", func() {
+	var appName string
+
+	BeforeEach(func() {
+		appName = generator.RandomName()
+	})
+
+	AfterEach(func() {
+		Expect(cf.Cf("delete", appName, "-f").Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+	})
+
+	It("Can mount a fuse endpoint", func() {
+		Expect(cf.Cf("push", appName, "-p", helpers.NewAssets().Fuse).Wait(CF_PUSH_TIMEOUT)).To(Exit(0))
+
+		Eventually(func() string {
+			return helpers.CurlAppRoot(appName)
+		}, DEFAULT_TIMEOUT).Should(ContainSubstring("great success!"))
+	})
+})
