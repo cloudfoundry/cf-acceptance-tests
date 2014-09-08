d85669fc2b1fd854e47ae785ce61042c275c9258
Amit Gupta <agupta@pivotallabs.com>
2014-09-08 16:57:10 -0700

    Double timeout for all-buildpack diego test

--- diego/lifecycle_test.go
+++ diego/lifecycle_test.go
@@ -19,7 +19,7 @@ var _ = Describe("Application Lifecycle", func() {
 			appName = generator.RandomName()
 			Eventually(cf.Cf("push", appName, "-p", helpers.NewAssets().Dora, "--no-start"), CF_PUSH_TIMEOUT).Should(Exit(0))
 			Eventually(cf.Cf("set-env", appName, "CF_DIEGO_RUN_BETA", "true"), DEFAULT_TIMEOUT).Should(Exit(0)) // CF_DIEGO_RUN_BETA also implies CF_DIEGO_BETA in CC
-			Eventually(cf.Cf("start", appName), CF_PUSH_TIMEOUT).Should(Exit(0))
+			Eventually(cf.Cf("start", appName), 2*CF_PUSH_TIMEOUT).Should(Exit(0))                              // Double timeout to allow for all buildpacks.
 		})
 
 		AfterEach(func() {
