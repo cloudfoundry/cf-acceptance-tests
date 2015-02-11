9cec7c35bf15c69434cf4912e4624a87ec30be97
Chris Piraino and Dan Lavine <cpiraino@pivotal.io>
2015-02-10 17:47:03 -0800

    Gofmt

--- v3/package_staging_test.go
+++ v3/package_staging_test.go
@@ -63,37 +63,37 @@ exit 1
 	var spaceGuid string
 	var token string
 
-		BeforeEach(func() {
+	BeforeEach(func() {
 		appName = generator.RandomName()
 
 		buildpackName = generator.RandomName()
 		buildpackZip := createBuildpack()
 
 		cf.AsUser(context.AdminUserContext(), func() {
-				Expect(cf.Cf("create-buildpack", buildpackName, buildpackZip, "999").Wait(DEFAULT_TIMEOUT)).To(Exit(0))
-			})
+			Expect(cf.Cf("create-buildpack", buildpackName, buildpackZip, "999").Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+		})
 
 		session := cf.Cf("curl", fmt.Sprintf("/v2/spaces?q=name:%s", context.RegularUserContext().Space))
 		bytes := session.Wait().Out.Contents()
 		var space struct {
-				Resources []struct {
+			Resources []struct {
 				Metadata struct {
 					Guid string `json:"guid"`
 				} `json:"metadata"`
 			} `json:"resources"`
-			}
+		}
 		json.Unmarshal(bytes, &space)
 		spaceGuid = space.Resources[0].Metadata.Guid
 
 		session = cf.Cf("curl", fmt.Sprintf("/v2/buildpacks?q=name:%s", buildpackName))
 		bytes = session.Wait().Out.Contents()
 		var buildpack struct {
-				Resources []struct {
+			Resources []struct {
 				Metadata struct {
 					Guid string `json:"guid"`
 				} `json:"metadata"`
 			} `json:"resources"`
-			}
+		}
 		json.Unmarshal(bytes, &buildpack)
 		buildpackGuid = buildpack.Resources[0].Metadata.Guid
 
@@ -101,8 +101,8 @@ exit 1
 		session = cf.Cf("curl", "/v3/apps", "-X", "POST", "-d", fmt.Sprintf(`{"name":"%s", "space_guid":"%s"}`, appName, spaceGuid))
 		bytes = session.Wait().Out.Contents()
 		var app struct {
-				Guid string `json:"guid"`
-			}
+			Guid string `json:"guid"`
+		}
 		json.Unmarshal(bytes, &app)
 		appGuid = app.Guid
 
@@ -111,8 +111,8 @@ exit 1
 		session = cf.Cf("curl", packageCreateUrl, "-X", "POST", "-d", fmt.Sprintf(`{"type":"bits"}`))
 		bytes = session.Wait().Out.Contents()
 		var pac struct {
-				Guid string `json:"guid"`
-			}
+			Guid string `json:"guid"`
+		}
 		json.Unmarshal(bytes, &pac)
 		packageGuid = pac.Guid
 
@@ -123,15 +123,15 @@ exit 1
 		bytes, _ = exec.Command("curl", "-v", "-s", uploadUrl, "-F", `bits=@"/Users/pivotal/workspace/cf-release/src/acceptance-tests/dora.zip"`, "-H", fmt.Sprintf("Authorization: %s", token)).CombinedOutput()
 		pkgUrl := fmt.Sprintf("/v3/packages/%s", packageGuid)
 		Eventually(func() *Session {
-				session = cf.Cf("curl", pkgUrl)
-				Expect(session.Wait(DEFAULT_TIMEOUT)).To(Exit(0))
-				return session
-			}, 1 * time.Minute).Should(Say("READY"))
+			session = cf.Cf("curl", pkgUrl)
+			Expect(session.Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+			return session
+		}, 1*time.Minute).Should(Say("READY"))
 	})
 
 	AfterEach(func() {
 		cf.AsUser(context.AdminUserContext(), func() {
-				Expect(cf.Cf("delete-buildpack", buildpackName, "-f").Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+			Expect(cf.Cf("delete-buildpack", buildpackName, "-f").Wait(DEFAULT_TIMEOUT)).To(Exit(0))
 		})
 	})
 
@@ -141,8 +141,8 @@ exit 1
 		session := cf.Cf("curl", stageUrl, "-X", "POST", "-d", fmt.Sprintf(`{"buildpack_guid":"%s"}`, buildpackGuid))
 		bytes := session.Wait().Out.Contents()
 		var droplet struct {
-				Guid string `json:"guid"`
-			}
+			Guid string `json:"guid"`
+		}
 		json.Unmarshal(bytes, &droplet)
 		dropletGuid := droplet.Guid
 		fmt.Println(string(bytes))
@@ -152,7 +152,7 @@ exit 1
 			session = runner.Curl(logUrl, "-H", fmt.Sprintf("Authorization: %s", token))
 			Expect(session.Wait(DEFAULT_TIMEOUT)).To(Exit(0))
 			return session
-		}, 1 * time.Minute, 10 * time.Second).Should(Say("STAGED WITH CUSTOM BUILDPACK"))
+		}, 1*time.Minute, 10*time.Second).Should(Say("STAGED WITH CUSTOM BUILDPACK"))
 	})
 
 	It("Stages with a user specified admin buildpack", func() {
@@ -161,18 +161,18 @@ exit 1
 		session := cf.Cf("curl", stageUrl, "-X", "POST", "-d", `{"buildpack_git_url":"http://github.com/cloudfoundry/go-buildpack"}`)
 		bytes := session.Wait().Out.Contents()
 		var droplet struct {
-				Guid string `json:"guid"`
-			}
+			Guid string `json:"guid"`
+		}
 		json.Unmarshal(bytes, &droplet)
 		dropletGuid := droplet.Guid
 		fmt.Println(string(bytes))
 
 		logUrl := fmt.Sprintf("loggregator.%s/recent?app=%s", config.AppsDomain, dropletGuid)
 		Eventually(func() *Session {
-				session = runner.Curl(logUrl, "-H", fmt.Sprintf("Authorization: %s", token))
-				Expect(session.Wait(DEFAULT_TIMEOUT)).To(Exit(0))
-				fmt.Println(string(session.Out.Contents()))
-				return session
-			}, 3 * time.Minute, 10 * time.Second).Should(Say("Cloning into"))
+			session = runner.Curl(logUrl, "-H", fmt.Sprintf("Authorization: %s", token))
+			Expect(session.Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+			fmt.Println(string(session.Out.Contents()))
+			return session
+		}, 3*time.Minute, 10*time.Second).Should(Say("Cloning into"))
 	})
 })
