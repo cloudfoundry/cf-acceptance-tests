68b3752b1c1a6af0f17cdb8bb65e74d8bedfe66f
Dan Lavine and Zak Auerbach <dlavine@us.ibm.com>
2015-06-18 16:03:39 -0700

    Re-add cat for downloading droplet
    [#95890146]

---
+++ apps/droplet_download_test.go
@@ -0,0 +1,54 @@
+package apps
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
+
+	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
+	"github.com/cloudfoundry/cf-acceptance-tests/helpers/assets"
+)
+
+var _ = Describe("Downloading droplets", func() {
+	var helloWorldAppName string
+	var out bytes.Buffer
+
+	BeforeEach(func() {
+		helloWorldAppName = generator.RandomName()
+
+		Expect(cf.Cf("push", helloWorldAppName, "-p", assets.NewAssets().HelloWorld).Wait(CF_PUSH_TIMEOUT)).To(Exit(0))
+	})
+
+	AfterEach(func() {
+		Expect(cf.Cf("delete", helloWorldAppName, "-f").Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+	})
+
+	It("Downloads the droplet for the app", func() {
+		guid := cf.Cf("app", helloWorldAppName, "--guid").Wait(DEFAULT_TIMEOUT).Out.Contents()
+		appGuid := strings.TrimSpace(string(guid))
+
+		tmpdir, err := ioutil.TempDir(os.TempDir(), "droplet-download")
+		Expect(err).ToNot(HaveOccurred())
+
+		app_droplet_path := path.Join(tmpdir, helloWorldAppName)
+
+		cf.Cf("curl", fmt.Sprintf("/v2/apps/%s/droplet/download", appGuid), "--output", app_droplet_path).Wait(DEFAULT_TIMEOUT)
+
+		cmd := exec.Command("tar", "-ztf", app_droplet_path)
+		cmd.Stdout = &out
+		err = cmd.Run()
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(out.String()).To(ContainSubstring("./app/config.ru"))
+		Expect(out.String()).To(ContainSubstring("./logs/staging_task.log"))
+	})
+})
