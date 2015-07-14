bab4ea075ce56df91c7e0b43be56fd9e2bcbb100
Greg Cobb and Jeff Pak <jeffrey.pak@emc.com>
2015-07-14 14:04:51 -0700

    Update recursive delete CAT with broker helper change
    [#97436908]

--- services/recursive_delete_test.go
+++ services/recursive_delete_test.go
@@ -17,7 +17,7 @@ var _ = Describe("Recursive Delete", func() {
 	var orgName string
 
 	BeforeEach(func() {
-		broker = NewServiceBroker(generator.RandomName(), assets.NewAssets().ServiceBroker, context)
+		broker = NewServiceBroker(generator.RandomName(), assets.NewAssets().ServiceBroker, context, false)
 		broker.Push()
 		broker.Configure()
 		broker.Create()
