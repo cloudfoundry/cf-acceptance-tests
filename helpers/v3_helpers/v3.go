887d2ad966297e4227e7c657eb4e228803a591c9
Joseph Palermo <jpalermo@pivotal.io>
2015-09-16 23:14:17 -0700

    Update v3 app creation to use new relationships syntax

--- helpers/v3_helpers/v3.go
+++ helpers/v3_helpers/v3.go
@@ -25,7 +25,7 @@ func StopApp(appGuid string) {
 }
 
 func CreateApp(appName, spaceGuid, environmentVariables string) string {
-	session := cf.Cf("curl", "/v3/apps", "-X", "POST", "-d", fmt.Sprintf(`{"name":"%s", "space_guid":"%s", "environment_variables":%s}`, appName, spaceGuid, environmentVariables))
+	session := cf.Cf("curl", "/v3/apps", "-X", "POST", "-d", fmt.Sprintf(`{"name":"%s", "relationships": {"space": {"guid": "%s"}}, "environment_variables":%s}`, appName, spaceGuid, environmentVariables))
 	bytes := session.Wait(DEFAULT_TIMEOUT).Out.Contents()
 	var app struct {
 		Guid string `json:"guid"`
