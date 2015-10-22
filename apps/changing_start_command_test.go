c771a154ba4d84c7c9638f39b51f64caa2fa5209
Luan Santos <lsantos@pivotal.io>
2015-10-22 09:46:35 -0700

    Remove process types tests
    Replaces them with a more specific start command coverage:
     - Procfile
     - -c flag
    
    [#103366472]
    
    Signed-off-by: Utako Ueda <uueda@pivotal.io>

--- apps/changing_start_command_test.go
+++ apps/changing_start_command_test.go
@@ -1,6 +1,9 @@
 package apps
 
 import (
+	"encoding/json"
+	"fmt"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gexec"
@@ -16,52 +19,85 @@ var _ = Describe("Changing an app's start command", func() {
 
 	BeforeEach(func() {
 		appName = generator.PrefixedRandomName("CATS-APP-")
-
-		Expect(cf.Cf(
-			"push", appName,
-			"-b", config.RubyBuildpackName,
-			"-m", "128M",
-			"-p", assets.NewAssets().Dora,
-			"-d", helpers.LoadConfig().AppsDomain,
-			"-c", "FOO=foo bundle exec rackup config.ru -p $PORT",
-		).Wait(CF_PUSH_TIMEOUT)).To(Exit(0))
 	})
 
 	AfterEach(func() {
 		Expect(cf.Cf("delete", appName, "-f").Wait(DEFAULT_TIMEOUT)).To(Exit(0))
 	})
 
-	It("takes effect after a restart, not requiring a push", func() {
-		Eventually(func() string {
-			return helpers.CurlApp(appName, "/env/FOO")
-		}, DEFAULT_TIMEOUT).Should(ContainSubstring("foo"))
+	Context("by using the command flag", func() {
 
-		var response cf.QueryResponse
+		BeforeEach(func() {
+			Expect(cf.Cf(
+				"push", appName,
+				"-b", config.RubyBuildpackName,
+				"-m", "128M",
+				"-p", assets.NewAssets().Dora,
+				"-d", helpers.LoadConfig().AppsDomain,
+				"-c", "FOO=foo bundle exec rackup config.ru -p $PORT",
+			).Wait(CF_PUSH_TIMEOUT)).To(Exit(0))
+		})
 
-		cf.ApiRequest("GET", "/v2/apps?q=name:"+appName, &response, DEFAULT_TIMEOUT)
+		It("takes effect after a restart, not requiring a push", func() {
+			Eventually(func() string {
+				return helpers.CurlApp(appName, "/env/FOO")
+			}, DEFAULT_TIMEOUT).Should(ContainSubstring("foo"))
 
-		Expect(response.Resources).To(HaveLen(1))
+			var response cf.QueryResponse
 
-		appGuid := response.Resources[0].Metadata.Guid
+			cf.ApiRequest("GET", "/v2/apps?q=name:"+appName, &response, DEFAULT_TIMEOUT)
 
-		cf.ApiRequest(
-			"PUT",
-			"/v2/apps/"+appGuid,
-			nil,
-			DEFAULT_TIMEOUT,
-			`{"command":"FOO=bar bundle exec rackup config.ru -p $PORT"}`,
-		)
+			Expect(response.Resources).To(HaveLen(1))
 
-		Expect(cf.Cf("stop", appName).Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+			appGuid := response.Resources[0].Metadata.Guid
 
-		Eventually(func() string {
-			return helpers.CurlApp(appName, "/env/FOO")
-		}, DEFAULT_TIMEOUT).Should(ContainSubstring("404"))
+			cf.ApiRequest(
+				"PUT",
+				"/v2/apps/"+appGuid,
+				nil,
+				DEFAULT_TIMEOUT,
+				`{"command":"FOO=bar bundle exec rackup config.ru -p $PORT"}`,
+			)
 
-		Expect(cf.Cf("start", appName).Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+			Expect(cf.Cf("stop", appName).Wait(DEFAULT_TIMEOUT)).To(Exit(0))
 
-		Eventually(func() string {
-			return helpers.CurlApp(appName, "/env/FOO")
-		}, DEFAULT_TIMEOUT).Should(ContainSubstring("bar"))
+			Eventually(func() string {
+				return helpers.CurlApp(appName, "/env/FOO")
+			}, DEFAULT_TIMEOUT).Should(ContainSubstring("404"))
+
+			Expect(cf.Cf("start", appName).Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+
+			Eventually(func() string {
+				return helpers.CurlApp(appName, "/env/FOO")
+			}, DEFAULT_TIMEOUT).Should(ContainSubstring("bar"))
+		})
+	})
+
+	Context("by using a Procfile", func() {
+		type AppResource struct {
+			Entity struct {
+				DetectedStartCommand string `json:"detected_start_command"`
+			} `json:"entity"`
+		}
+
+		type AppsResponse struct {
+			Resources []AppResource `json:"resources"`
+		}
+
+		BeforeEach(func() {
+			Expect(cf.Cf("push", appName, "-b", config.NodejsBuildpackName, "-m", "128M", "-p", assets.NewAssets().NodeWithProcfile, "-d", config.AppsDomain).Wait(CF_PUSH_TIMEOUT)).To(Exit(0))
+		})
+
+		AfterEach(func() {
+			Expect(cf.Cf("delete", appName, "-f").Wait(CF_PUSH_TIMEOUT)).To(Exit(0))
+		})
+
+		It("detects the use of the start command in the 'web' process type", func() {
+			var appsResponse AppsResponse
+			cfResponse := cf.Cf("curl", fmt.Sprintf("/v2/apps?q=name:%s", appName)).Wait(DEFAULT_TIMEOUT).Out.Contents()
+			json.Unmarshal(cfResponse, &appsResponse)
+
+			Expect(appsResponse.Resources[0].Entity.DetectedStartCommand).To(Equal("node app.js"))
+		})
 	})
 })
