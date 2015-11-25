e3129458251047486dac8173e87f039ee24577fb
Atul Kshirsagar <atul.kshirsagar@gmail.com>
2015-11-25 10:37:52 -0800

    Add CATS test for verifying sse events from routing-api.
    [#99063218]
    
    Signed-off-by: Fermin Ordaz <fermin.ordaz@gmail.com>

--- routing/registration_test.go
+++ routing/registration_test.go
@@ -1,9 +1,12 @@
 package routing
 
 import (
+	"time"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
 
 	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
 )
@@ -24,16 +27,36 @@ var _ = Describe("Registration", func() {
 		oauthPassword = config.ClientSecret
 		oauthUrl = config.Protocol() + "uaa." + systemDomain
 		routingApiEndpoint = config.Protocol() + "api." + systemDomain
-
-		route = generator.RandomName()
-		routeJSON = `[{"route":"` + route + `","port":65340,"ip":"1.2.3.4","ttl":60}]`
 	})
 
-	Describe("the routing API is enabled", func() {
-		It("can register, list and unregister routes", func() {
-			args := []string{"register", routeJSON, "--api", routingApiEndpoint, "--client-id", "gorouter", "--client-secret", oauthPassword, "--oauth-url", oauthUrl}
+	Describe("HTTP Route", func() {
+		var (
+			eventsSession *Session
+		)
+		BeforeEach(func() {
+			route = generator.RandomName()
+			routeJSON = `[{"route":"` + route + `","port":65340,"ip":"1.2.3.4","ttl":60}]`
+		})
+
+		AfterEach(func() {
+			if eventsSession != nil {
+				eventsSession.Kill()
+			}
+		})
+
+		It("can register, list, subscribe to sse and unregister routes", func() {
+			args := []string{"events", "--http", "--api", routingApiEndpoint, "--client-id", "gorouter", "--client-secret", oauthPassword, "--oauth-url", oauthUrl}
+			eventsSession = Rtr(args...)
+			Eventually(eventsSession.Out, 70*time.Second).Should(Say("api." + systemDomain))
+
+			args = []string{"register", routeJSON, "--api", routingApiEndpoint, "--client-id", "gorouter", "--client-secret", oauthPassword, "--oauth-url", oauthUrl}
 			session := Rtr(args...)
 			Eventually(session.Out).Should(Say("Successfully registered routes"))
+			Eventually(eventsSession.Out, 10*time.Second).Should(Say(route))
+			Eventually(eventsSession.Out).Should(Say(`"port":65340`))
+			Eventually(eventsSession.Out).Should(Say(`"ip":"1.2.3.4"`))
+			Eventually(eventsSession.Out).Should(Say(`"ttl":60`))
+			Eventually(eventsSession.Out).Should(Say(`"Action":"Upsert"`))
 
 			args = []string{"list", "--api", routingApiEndpoint, "--client-id", "gorouter", "--client-secret", oauthPassword, "--oauth-url", oauthUrl}
 			session = Rtr(args...)
