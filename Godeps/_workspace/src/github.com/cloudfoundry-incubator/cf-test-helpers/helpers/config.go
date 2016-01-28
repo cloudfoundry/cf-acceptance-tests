99d988d732d02d78b72af30b1f3bdc631dad8ad3
David Sabeti <dsabeti@pivotal.io>
2016-01-28 14:15:11 -0800

    Update cf-test-helpers

--- Godeps/_workspace/src/github.com/cloudfoundry-incubator/cf-test-helpers/helpers/config.go
+++ Godeps/_workspace/src/github.com/cloudfoundry-incubator/cf-test-helpers/helpers/config.go
@@ -31,12 +31,11 @@ type Config struct {
 	PersistentAppOrg       string `json:"persistent_app_org"`
 	PersistentAppQuotaName string `json:"persistent_app_quota_name"`
 
-	SkipSSLValidation     bool   `json:"skip_ssl_validation"`
-	SkipSSHHostValidation bool   `json:"skip_ssh_host_validation"`
-	Backend               string `json:"backend"`
-	IncludeRouteServices  bool   `json:"include_route_services"`
-	IncludeDiegoDocker    bool   `json:"include_diego_docker"`
-	IncludeTasks          bool   `json:"include_tasks"`
+	SkipSSLValidation    bool   `json:"skip_ssl_validation"`
+	Backend              string `json:"backend"`
+	IncludeRouteServices bool   `json:"include_route_services"`
+	IncludeDiegoDocker   bool   `json:"include_diego_docker"`
+	IncludeTasks         bool   `json:"include_tasks"`
 
 	ArtifactsDirectory string `json:"artifacts_directory"`
 
