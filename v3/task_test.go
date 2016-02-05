5c66439c7ec1bb0c9737b83bc94e9e500117138b
Simon Leung <leungs@us.ibm.com>
2016-02-05 10:53:00 -0800

    Add test for canceling a v3 task
    [#111473134]
    
    Signed-off-by: Kara Felix <kfelix@us.ibm.com>

--- v3/task_test.go
+++ v3/task_test.go
@@ -16,44 +16,50 @@ import (
 )
 
 var _ = Describe("v3 tasks", func() {
+	type Result struct {
+		FailureReason string `json: "failure_reason"`
+	}
+
+	type Task struct {
+		Guid    string `json:"guid"`
+		Command string `json:"command"`
+		Name    string `json:"name"`
+		State   string `json:"state"`
+		Result  Result `json:"result"`
+	}
+
+	var (
+		appName                         string
+		appGuid                         string
+		packageGuid                     string
+		spaceGuid                       string
+		appCreationEnvironmentVariables string
+	)
+
+	BeforeEach(func() {
+		appName = generator.PrefixedRandomName("CATS-APP-")
+		spaceGuid = GetSpaceGuidFromName(context.RegularUserContext().Space)
+		appCreationEnvironmentVariables = `"foo"=>"bar"`
+		appGuid = CreateApp(appName, spaceGuid, `{"foo":"bar"}`)
+		packageGuid = CreatePackage(appGuid)
+		token := GetAuthToken()
+		uploadUrl := fmt.Sprintf("%s/v3/packages/%s/upload", config.ApiEndpoint, packageGuid)
+		UploadPackage(uploadUrl, assets.NewAssets().DoraZip, token)
+		WaitForPackageToBeReady(packageGuid)
+		dropletGuid := StagePackage(packageGuid, "{}")
+		WaitForDropletToStage(dropletGuid)
+		AssignDropletToApp(appGuid, dropletGuid)
+	})
+
+	AfterEach(func() {
+		DeleteApp(appGuid)
+	})
+
 	config := helpers.LoadConfig()
+
 	if config.IncludeTasks {
 		Context("tasks lifecycle", func() {
-			var (
-				appName                         string
-				appGuid                         string
-				packageGuid                     string
-				spaceGuid                       string
-				appCreationEnvironmentVariables string
-			)
-
-			BeforeEach(func() {
-				appName = generator.PrefixedRandomName("CATS-APP-")
-				spaceGuid = GetSpaceGuidFromName(context.RegularUserContext().Space)
-				appCreationEnvironmentVariables = `"foo"=>"bar"`
-				appGuid = CreateApp(appName, spaceGuid, `{"foo":"bar"}`)
-				packageGuid = CreatePackage(appGuid)
-				token := GetAuthToken()
-				uploadUrl := fmt.Sprintf("%s/v3/packages/%s/upload", config.ApiEndpoint, packageGuid)
-				UploadPackage(uploadUrl, assets.NewAssets().DoraZip, token)
-				WaitForPackageToBeReady(packageGuid)
-				dropletGuid := StagePackage(packageGuid, "{}")
-				WaitForDropletToStage(dropletGuid)
-				AssignDropletToApp(appGuid, dropletGuid)
-			})
-
-			AfterEach(func() {
-				DeleteApp(appGuid)
-			})
-
 			It("can successfully create and run a task", func() {
-				type Task struct {
-					Guid    string `json:"guid"`
-					Command string `json:"command"`
-					Name    string `json:"name"`
-					State   string `json:"state"`
-				}
-
 				By("creating the task")
 				var createOutput Task
 				postBody := `{"command": "echo 0", "name": "mreow"}`
@@ -82,6 +88,27 @@ var _ = Describe("v3 tasks", func() {
 				usageEvents = LastPageUsageEvents(context)
 				stop_event := AppUsageEvent{Entity{State: "TASK_STOPPED", ParentAppGuid: appGuid, ParentAppName: appName, TaskGuid: createOutput.Guid}}
 				Expect(UsageEventsInclude(usageEvents, stop_event)).To(BeTrue())
+			})
+		})
+
+		Context("When canceling a task", func() {
+			var createOutput Task
+
+			BeforeEach(func() {
+				postBody := `{"command": "sleep 100;", "name": "mreow"}`
+				createCommand := cf.Cf("curl", fmt.Sprintf("/v3/apps/%s/tasks", appGuid), "-X", "POST", "-d", postBody).Wait(DEFAULT_TIMEOUT)
+				Expect(createCommand).To(Exit(0))
+			})
+
+			It("should show task is in FAILED state", func() {
+				var cancelOutput Task
+				Eventually(func() string {
+					cancelCommand := cf.Cf("curl", fmt.Sprintf("/v3/apps/%s/tasks/%s/cancel", appGuid, createOutput.Guid), "-X", "PUT").Wait(DEFAULT_TIMEOUT)
+					Expect(cancelCommand).To(Exit(0))
+					json.Unmarshal(cancelCommand.Out.Contents(), &cancelOutput)
+					return cancelOutput.State
+				}, DEFAULT_TIMEOUT).Should(Equal("FAILED"))
+				Expect(cancelOutput.Result.FailureReason).To(Equal("task was canceled"))
 			})
 		})
 	}
