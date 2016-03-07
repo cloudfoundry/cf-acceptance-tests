472a6bdb72a26f7ae53243a0b8994927b05f202a
Zach <zach.robinson@gmail.com>
2016-03-06 16:51:24 -0800

    Revert "temporarily pend task canceling tests"
    This reverts commit 4cb3e0ec62f477a35efb688e97c3cd456a8693e5.

--- v3/task_test.go
+++ v3/task_test.go
@@ -93,7 +93,7 @@ var _ = Describe("v3 tasks", func() {
 			})
 		})
 
-		XContext("When canceling a task", func() {
+		Context("When canceling a task", func() {
 			var taskGuid string
 
 			BeforeEach(func() {
