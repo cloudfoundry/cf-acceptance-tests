5a5790c496e8514de830525d9311d173a5961fdf
Jeff Pak <jeffrey.pak@emc.com>
2016-03-08 16:51:28 -0800

    add tests for purging service instances
    [#114707489]
    
    Signed-off-by: Zach Robinson <zrobinson@pivotal.io>

---
+++ services/purge_service_instance_test.go
@@ -0,0 +1,131 @@
+package services_test
+
+import (
+	. "github.com/cloudfoundry/cf-acceptance-tests/Godeps/_workspace/src/github.com/onsi/ginkgo"
+	. "github.com/cloudfoundry/cf-acceptance-tests/Godeps/_workspace/src/github.com/onsi/gomega"
+	. "github.com/cloudfoundry/cf-acceptance-tests/Godeps/_workspace/src/github.com/onsi/gomega/gbytes"
+	. "github.com/cloudfoundry/cf-acceptance-tests/Godeps/_workspace/src/github.com/onsi/gomega/gexec"
+
+	"github.com/cloudfoundry/cf-acceptance-tests/Godeps/_workspace/src/github.com/cloudfoundry-incubator/cf-test-helpers/cf"
+	"github.com/cloudfoundry/cf-acceptance-tests/Godeps/_workspace/src/github.com/cloudfoundry-incubator/cf-test-helpers/generator"
+	"github.com/cloudfoundry/cf-acceptance-tests/helpers/app_helpers"
+	"github.com/cloudfoundry/cf-acceptance-tests/helpers/assets"
+	. "github.com/cloudfoundry/cf-acceptance-tests/helpers/services"
+)
+
+var _ = Describe("Purging service instances", func() {
+	var broker ServiceBroker
+	var appName, instanceName string
+
+	AfterEach(func() {
+		app_helpers.AppReport(broker.Name, DEFAULT_TIMEOUT)
+		broker.Destroy()
+	})
+
+	Context("for public brokers", func() {
+		BeforeEach(func() {
+			broker = NewServiceBroker(
+				generator.PrefixedRandomName("psi-"),
+				assets.NewAssets().ServiceBroker,
+				context,
+			)
+			broker.Push()
+			broker.Configure()
+			cf.AsUser(context.AdminUserContext(), DEFAULT_TIMEOUT, func() {
+				broker.Create()
+				broker.PublicizePlans()
+			})
+			appName = generator.PrefixedRandomName("CATS-APP-psi-")
+			instanceName = generator.PrefixedRandomName("CATS-SI-psi-")
+		})
+
+		AfterEach(func() {
+			app_helpers.AppReport(appName, DEFAULT_TIMEOUT)
+			Expect(cf.Cf("delete", appName, "-f", "-r").Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+		})
+
+		It("removes the service instance", func() {
+			By("Having a bound service instance")
+			createApp := cf.Cf("push", appName, "--no-start", "-b", config.RubyBuildpackName, "-m", DEFAULT_MEMORY_LIMIT, "-p", assets.NewAssets().Dora, "-d", config.AppsDomain).Wait(DEFAULT_TIMEOUT)
+			Expect(createApp).To(Exit(0), "failed creating app")
+			app_helpers.SetBackend(appName)
+			Expect(cf.Cf("start", appName).Wait(CF_PUSH_TIMEOUT)).To(Exit(0))
+
+			broker.CreateServiceInstance(instanceName)
+
+			services := cf.Cf("services").Wait(DEFAULT_TIMEOUT)
+			Expect(services).To(Exit(0))
+			Expect(services).To(Say(instanceName))
+
+			bindService := cf.Cf("bind-service", appName, instanceName).Wait(DEFAULT_TIMEOUT)
+			Expect(bindService).To(Exit(0), "failed binding app to service")
+
+			By("Making the broker unavailable")
+			Expect(cf.Cf("delete", broker.Name, "-f", "-r").Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+
+			By("Purging the service instance")
+			cf.AsUser(context.AdminUserContext(), DEFAULT_TIMEOUT, func() {
+				cf.TargetSpace(context.RegularUserContext(), context.ShortTimeout())
+				Expect(cf.Cf("purge-service-instance", instanceName, "-f").Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+			})
+
+			By("Ensuring the service instance is gone")
+			services = cf.Cf("services").Wait(DEFAULT_TIMEOUT)
+			Expect(services).To(Exit(0))
+			Expect(services).NotTo(Say(instanceName))
+			Expect(services).NotTo(Say(appName))
+		})
+	})
+
+	Context("for space scoped brokers", func() {
+		BeforeEach(func() {
+			broker = NewServiceBroker(
+				generator.PrefixedRandomName("prpsi-"),
+				assets.NewAssets().ServiceBroker,
+				context,
+			)
+			cf.TargetSpace(context.RegularUserContext(), context.ShortTimeout())
+			broker.Push()
+			broker.Configure()
+			broker.CreateSpaceScoped()
+			appName = generator.PrefixedRandomName("CATS-APP-prpsi-")
+			instanceName = generator.PrefixedRandomName("CATS-SI-prpsi-")
+		})
+
+		AfterEach(func() {
+			app_helpers.AppReport(appName, DEFAULT_TIMEOUT)
+			Expect(cf.Cf("delete", appName, "-f", "-r").Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+		})
+
+		It("removes the service instance", func() {
+			cf.AsUser(context.RegularUserContext(), context.ShortTimeout(), func() {
+				By("Having a bound service instance")
+				createApp := cf.Cf("push", appName, "--no-start", "-b", config.RubyBuildpackName, "-m", DEFAULT_MEMORY_LIMIT, "-p", assets.NewAssets().Dora, "-d", config.AppsDomain).Wait(DEFAULT_TIMEOUT)
+				Expect(createApp).To(Exit(0), "failed creating app")
+				app_helpers.SetBackend(appName)
+				Expect(cf.Cf("start", appName).Wait(CF_PUSH_TIMEOUT)).To(Exit(0))
+
+				broker.CreateServiceInstance(instanceName)
+
+				services := cf.Cf("services").Wait(DEFAULT_TIMEOUT)
+				Expect(services).To(Exit(0))
+				Expect(services).To(Say(instanceName))
+
+				bindService := cf.Cf("bind-service", appName, instanceName).Wait(DEFAULT_TIMEOUT)
+				Expect(bindService).To(Exit(0), "failed binding app to service")
+
+				By("Making the broker unavailable")
+				Expect(cf.Cf("delete", broker.Name, "-f", "-r").Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+
+				By("Purging the service instance")
+				Expect(cf.Cf("purge-service-instance", instanceName, "-f").Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+
+				By("Ensuring the service instance is gone")
+				services = cf.Cf("services").Wait(DEFAULT_TIMEOUT)
+				Expect(services).To(Exit(0))
+				Expect(services).NotTo(Say(instanceName))
+				Expect(services).NotTo(Say(appName))
+			})
+		})
+	})
+})
