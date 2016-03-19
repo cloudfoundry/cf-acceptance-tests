3bcd58c4f57e3c0f12fc07bac199c58fac73f968
David Sabeti <dsabeti@pivotal.io>
2016-03-18 17:18:31 -0700

    Use randomized values for env vars in operator tests

--- operator/environment_variables_group_test.go
+++ operator/environment_variables_group_test.go
@@ -1,8 +1,11 @@
 package operator
 
 import (
+	"fmt"
 	"io/ioutil"
 	"path"
+	"strconv"
+	"time"
 
 	"github.com/cloudfoundry/cf-acceptance-tests/Godeps/_workspace/src/github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	"github.com/cloudfoundry/cf-acceptance-tests/Godeps/_workspace/src/github.com/cloudfoundry-incubator/cf-test-helpers/generator"
@@ -19,7 +22,7 @@ import (
 )
 
 var _ = Describe("Environment Variables Groups", func() {
-	var createBuildpack = func() string {
+	var createBuildpack = func(envVarName string) string {
 		tmpPath, err := ioutil.TempDir("", "env-group-staging")
 		Expect(err).ToNot(HaveOccurred())
 
@@ -28,11 +31,11 @@ var _ = Describe("Environment Variables Groups", func() {
 		archive_helpers.CreateZipArchive(buildpackArchivePath, []archive_helpers.ArchiveFile{
 			{
 				Name: "bin/compile",
-				Body: `#!/usr/bin/env bash
+				Body: fmt.Sprintf(`#!/usr/bin/env bash
 sleep 5
-echo $CATS_STAGING_TEST_VAR
+echo $%s
 exit 1
-`,
+`, envVarName),
 			},
 			{
 				Name: "bin/detect",
@@ -55,9 +58,13 @@ exit 1
 		var originalStagingEnv string
 		var appName string
 		var buildpackName string
+		var envVarName, envVarValue string
 
 		BeforeEach(func() {
 			appName = generator.PrefixedRandomName("CATS-APP-")
+			envVarName = fmt.Sprintf("CATS_STAGING_TEST_VAR_%s", strconv.Itoa(int(time.Now().UnixNano())))
+			envVarValue = fmt.Sprintf("staging_env_value_%s", strconv.Itoa(int(time.Now().UnixNano())))
+
 			cf.AsUser(context.AdminUserContext(), DEFAULT_TIMEOUT, func() {
 				session := cf.Cf("curl", "/v2/config/environment_variable_groups/staging").Wait(DEFAULT_TIMEOUT)
 				Expect(session).To(Exit(0))
@@ -81,10 +88,11 @@ exit 1
 
 		It("Applies environment variables while staging apps", func() {
 			buildpackName = generator.RandomName()
-			buildpackZip := createBuildpack()
+			buildpackZip := createBuildpack(envVarName)
 
 			cf.AsUser(context.AdminUserContext(), DEFAULT_TIMEOUT, func() {
-				Expect(cf.Cf("set-staging-environment-variable-group", `{"CATS_STAGING_TEST_VAR":"staging_env_value"}`).Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+				env := fmt.Sprintf(`{"%s": "%s"}`, envVarName, envVarValue)
+				Expect(cf.Cf("set-staging-environment-variable-group", env).Wait(DEFAULT_TIMEOUT)).To(Exit(0))
 				Expect(cf.Cf("create-buildpack", buildpackName, buildpackZip, "999").Wait(DEFAULT_TIMEOUT)).To(Exit(0))
 			})
 
@@ -96,15 +104,19 @@ exit 1
 				appLogsSession := cf.Cf("logs", "--recent", appName)
 				Expect(appLogsSession.Wait(DEFAULT_TIMEOUT)).To(Exit(0))
 				return appLogsSession
-			}, DEFAULT_TIMEOUT).Should(Say("staging_env_value"))
+			}, DEFAULT_TIMEOUT).Should(Say(envVarValue))
 		})
 	})
 	Context("Running environment variable groups", func() {
 		var originalRunningEnv string
 		var appName string
+		var envVarName, envVarValue string
 
 		BeforeEach(func() {
 			appName = generator.PrefixedRandomName("CATS-APP-")
+			envVarName = fmt.Sprintf("CATS_RUNNING_TEST_VAR_%s", strconv.Itoa(int(time.Now().UnixNano())))
+			envVarValue = fmt.Sprintf("running_env_value_%s", strconv.Itoa(int(time.Now().UnixNano())))
+
 			cf.AsUser(context.AdminUserContext(), DEFAULT_TIMEOUT, func() {
 				session := cf.Cf("curl", "/v2/config/environment_variable_groups/running").Wait(DEFAULT_TIMEOUT)
 				Expect(session).To(Exit(0))
@@ -124,7 +136,8 @@ exit 1
 
 		It("Applies correct environment variables while running apps", func() {
 			cf.AsUser(context.AdminUserContext(), DEFAULT_TIMEOUT, func() {
-				Expect(cf.Cf("set-running-environment-variable-group", `{"CATS_RUNNING_TEST_VAR":"running_env_value"}`).Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+				env := fmt.Sprintf(`{"%s": "%s"}`, envVarName, envVarValue)
+				Expect(cf.Cf("set-running-environment-variable-group", env).Wait(DEFAULT_TIMEOUT)).To(Exit(0))
 			})
 
 			Expect(cf.Cf("push", appName, "--no-start", "-b", config.RubyBuildpackName, "-m", DEFAULT_MEMORY_LIMIT, "-p", assets.NewAssets().Dora, "-d", config.AppsDomain).Wait(DEFAULT_TIMEOUT)).To(Exit(0))
@@ -133,7 +146,7 @@ exit 1
 
 			env := helpers.CurlApp(appName, "/env")
 
-			Expect(env).To(ContainSubstring("running_env_value"))
+			Expect(env).To(ContainSubstring(envVarValue))
 		})
 	})
 })
