58c26aaf51aa474891112b74859fe29f4865d242
Utako Ueda <uueda@pivotal.io>
2016-03-21 09:59:54 -0700

    move inigo test for buildpacks
    [#113941499]
    
    Signed-off-by: Dan Lavine <dlavine@us.ibm.com>

--- apps/admin_buildpack_lifecycle_test.go
+++ apps/admin_buildpack_lifecycle_test.go
@@ -39,7 +39,7 @@ var _ = Describe("Admin Buildpacks", func() {
 		Empty bool
 	}
 
-	setupBuildpack := func(appConfig appConfig) {
+	setupBadDetectBuildpack := func(appConfig appConfig) {
 		cf.AsUser(context.AdminUserContext(), DEFAULT_TIMEOUT, func() {
 			BuildpackName = RandomName()
 			appName = PrefixedRandomName("CATS-APP-")
@@ -91,6 +91,127 @@ config_vars:
 default_process_types:
   web: while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; echo "hi from a simple admin buildpack"; } | nc -l \$PORT; done
 EOF
+`,
+				},
+			})
+
+			if !appConfig.Empty {
+				_, err = os.Create(path.Join(appPath, matchingFilename(appName)))
+				Expect(err).ToNot(HaveOccurred())
+			}
+
+			_, err = os.Create(path.Join(appPath, "some-file"))
+			Expect(err).ToNot(HaveOccurred())
+
+			createBuildpack := cf.Cf("create-buildpack", BuildpackName, buildpackArchivePath, "0").Wait(DEFAULT_TIMEOUT)
+			Expect(createBuildpack).Should(Exit(0))
+			Expect(createBuildpack).Should(Say("Creating"))
+			Expect(createBuildpack).Should(Say("OK"))
+			Expect(createBuildpack).Should(Say("Uploading"))
+			Expect(createBuildpack).Should(Say("OK"))
+		})
+	}
+
+	setupBadCompileBuildpack := func(appConfig appConfig) {
+		cf.AsUser(context.AdminUserContext(), DEFAULT_TIMEOUT, func() {
+			BuildpackName = RandomName()
+			appName = PrefixedRandomName("CATS-APP-")
+
+			tmpdir, err := ioutil.TempDir(os.TempDir(), "matching-app")
+			Expect(err).ToNot(HaveOccurred())
+
+			appPath = tmpdir
+
+			tmpdir, err = ioutil.TempDir(os.TempDir(), "matching-buildpack")
+			Expect(err).ToNot(HaveOccurred())
+
+			buildpackPath = tmpdir
+			buildpackArchivePath = path.Join(buildpackPath, "buildpack.zip")
+
+			archive_helpers.CreateZipArchive(buildpackArchivePath, []archive_helpers.ArchiveFile{
+				{
+					Name: "bin/compile",
+					Body: `#!/usr/bin/env bash
+
+exit 1
+`,
+				},
+				{
+					Name: "bin/detect",
+					Body: fmt.Sprintf(`#!/bin/bash
+
+echo Simple
+`, matchingFilename(appName)),
+				},
+				{
+					Name: "bin/release",
+					Body: `#!/usr/bin/env bash
+
+cat <<EOF
+---
+config_vars:
+  PATH: bin:/usr/local/bin:/usr/bin:/bin
+  FROM_BUILD_PACK: "yes"
+default_process_types:
+  web: while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; echo "hi from a simple admin buildpack"; } | nc -l \$PORT; done
+EOF
+`,
+				},
+			})
+
+			if !appConfig.Empty {
+				_, err = os.Create(path.Join(appPath, matchingFilename(appName)))
+				Expect(err).ToNot(HaveOccurred())
+			}
+
+			_, err = os.Create(path.Join(appPath, "some-file"))
+			Expect(err).ToNot(HaveOccurred())
+
+			createBuildpack := cf.Cf("create-buildpack", BuildpackName, buildpackArchivePath, "0").Wait(DEFAULT_TIMEOUT)
+			Expect(createBuildpack).Should(Exit(0))
+			Expect(createBuildpack).Should(Say("Creating"))
+			Expect(createBuildpack).Should(Say("OK"))
+			Expect(createBuildpack).Should(Say("Uploading"))
+			Expect(createBuildpack).Should(Say("OK"))
+		})
+	}
+
+	setupBadReleaseBuildpack := func(appConfig appConfig) {
+		cf.AsUser(context.AdminUserContext(), DEFAULT_TIMEOUT, func() {
+			BuildpackName = RandomName()
+			appName = PrefixedRandomName("CATS-APP-")
+
+			tmpdir, err := ioutil.TempDir(os.TempDir(), "matching-app")
+			Expect(err).ToNot(HaveOccurred())
+
+			appPath = tmpdir
+
+			tmpdir, err = ioutil.TempDir(os.TempDir(), "matching-buildpack")
+			Expect(err).ToNot(HaveOccurred())
+
+			buildpackPath = tmpdir
+			buildpackArchivePath = path.Join(buildpackPath, "buildpack.zip")
+
+			archive_helpers.CreateZipArchive(buildpackArchivePath, []archive_helpers.ArchiveFile{
+				{
+					Name: "bin/compile",
+					Body: `#!/usr/bin/env bash
+
+echo Pass compile
+`,
+				},
+				{
+					Name: "bin/detect",
+					Body: fmt.Sprintf(`#!/bin/bash
+
+echo Pass Detect
+`, matchingFilename(appName)),
+				},
+				{
+					Name: "bin/release",
+					Body: `#!/usr/bin/env bash
+
+exit 1
 `,
 				},
 			})
@@ -181,29 +302,71 @@ EOF
 		Expect(start).To(Say("NoAppDetectedError"))
 	}
 
+	itRaisesBuildpackCompileFailedError := func() {
+		Expect(cf.Cf("push", appName, "--no-start", "-b", BuildpackName, "-m", DEFAULT_MEMORY_LIMIT, "-p", appPath, "-d", config.AppsDomain).Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+		app_helpers.SetBackend(appName)
+
+		start := cf.Cf("start", appName).Wait(CF_PUSH_TIMEOUT)
+		Expect(start).To(Exit(1))
+		Expect(start).To(Say("BuildpackCompileFailed"))
+	}
+
+	itRaisesBuildpackReleaseFailedError := func() {
+		Expect(cf.Cf("push", appName, "--no-start", "-b", BuildpackName, "-m", DEFAULT_MEMORY_LIMIT, "-p", appPath, "-d", config.AppsDomain).Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+		app_helpers.SetBackend(appName)
+
+		start := cf.Cf("start", appName).Wait(CF_PUSH_TIMEOUT)
+		Expect(start).To(Exit(1))
+		Expect(start).To(Say("BuildpackReleaseFailed"))
+	}
+
 	Context("when the buildpack is not specified", func() {
 		It("runs the app only if the buildpack is detected", func() {
 			// Tests that rely on buildpack detection must be run in serial,
 			// but ginkgo doesn't allow specific blocks to be marked as serial-only
 			// so we manually mimic setup/teardown pattern here
 
-			setupBuildpack(appConfig{Empty: false})
+			setupBadDetectBuildpack(appConfig{Empty: false})
 			itIsUsedForTheApp()
 			deleteApp()
 			deleteBuildpack()
 
-			setupBuildpack(appConfig{Empty: true})
+			setupBadDetectBuildpack(appConfig{Empty: true})
 			itDoesNotDetectForEmptyApp()
 			deleteApp()
 			deleteBuildpack()
 
-			setupBuildpack(appConfig{Empty: false})
+			setupBadDetectBuildpack(appConfig{Empty: false})
 			itDoesNotDetectWhenBuildpackDisabled()
 			deleteApp()
 			deleteBuildpack()
 
-			setupBuildpack(appConfig{Empty: false})
+			setupBadDetectBuildpack(appConfig{Empty: false})
 			itDoesNotDetectWhenBuildpackDeleted()
+			deleteApp()
+			deleteBuildpack()
+		})
+	})
+
+	Context("when the buildpack compile fails", func() {
+		// This test used to be part of inigo and with the extraction of CC bridge, we want to ensure
+		// that user facing errors are correctly propagated from a garden container out of the system.
+
+		It(diegoUnsupportedTag+"the user receives a BuildpackCompileFailed error", func() {
+			setupBadCompileBuildpack(appConfig{Empty: false})
+			itRaisesBuildpackCompileFailedError()
+			deleteApp()
+			deleteBuildpack()
+		})
+	})
+
+	Context("when the buildpack release fails", func() {
+		// This test used to be part of inigo and with the extraction of CC bridge, we want to ensure
+		// that user facing errors are correctly propagated from a garden container out of the system.
+
+		It(diegoUnsupportedTag+"the user receives a BuildpackReleaseFailed error", func() {
+			setupBadReleaseBuildpack(appConfig{Empty: false})
+			itRaisesBuildpackReleaseFailedError()
 			deleteApp()
 			deleteBuildpack()
 		})
