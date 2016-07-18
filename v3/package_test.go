49d86c231d91cfa7bb4cd588c40faf7ed9775a8a
Raina Masand <rmasand@pivotal.io>
2016-07-18 11:09:41 -0700

    Buildpack names are taken from config
    [#126327439]
    
    Signed-off-by: Ryan Moran <rmoran@pivotal.io>

--- v3/package_test.go
+++ v3/package_test.go
@@ -93,7 +93,7 @@ var _ = Describe("package features", func() {
 		})
 
 		It("can still stage the package", func() {
-			dropletGuid := StageBuildpackPackage(packageGuid, "java_buildpack")
+			dropletGuid := StageBuildpackPackage(packageGuid, config.JavaBuildpackName)
 			dropletPath := fmt.Sprintf("/v3/droplets/%s", dropletGuid)
 
 			Eventually(func() *Session {
