58b26b83b56dda6124af665bba0489ab59215fe0
Chunyi Lyu <clyu@pivotal.io>
2016-09-13 11:36:53 -0700

    Rename config.NodejsBuildpackName => Config.NodejsBuildpackName
    Signed-off-by: Joshua Aresty <joshua.aresty@emc.com>

--- apps/changing_start_command.go
+++ apps/changing_start_command.go
@@ -89,7 +89,7 @@ var _ = AppsDescribe("Changing an app's start command", func() {
 		}
 
 		BeforeEach(func() {
-			Expect(cf.Cf("push", appName, "--no-start", "-b", config.NodejsBuildpackName, "-m", DEFAULT_MEMORY_LIMIT, "-p", assets.NewAssets().NodeWithProcfile, "-d", Config.AppsDomain).Wait(DEFAULT_TIMEOUT)).To(Exit(0))
+			Expect(cf.Cf("push", appName, "--no-start", "-b", Config.NodejsBuildpackName, "-m", DEFAULT_MEMORY_LIMIT, "-p", assets.NewAssets().NodeWithProcfile, "-d", Config.AppsDomain).Wait(DEFAULT_TIMEOUT)).To(Exit(0))
 			app_helpers.SetBackend(appName)
 			Expect(cf.Cf("start", appName).Wait(CF_PUSH_TIMEOUT)).To(Exit(0))
 		})
