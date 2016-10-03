7d8d8803f9ca3f0c4be85429d6503a10a2d26532
Zach Robinson <zrobinson@pivotal.io>
2016-10-03 10:38:28 -0700

    Add test for uploading droplets
    [#128036647]
    
    Signed-off-by: Aakash Shah <ashah@pivotal.io>

--- apps/droplet_uploading_and_downloading.go
+++ apps/droplet_uploading_and_downloading.go
@@ -2,6 +2,7 @@ package apps
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -13,15 +14,18 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
 
 	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
 	"github.com/cloudfoundry/cf-acceptance-tests/helpers/app_helpers"
 	"github.com/cloudfoundry/cf-acceptance-tests/helpers/assets"
 	"github.com/cloudfoundry/cf-acceptance-tests/helpers/random_name"
+	"github.com/cloudfoundry/cf-acceptance-tests/helpers/v3_helpers"
 )
 
-var _ = AppsDescribe("Downloading droplets", func() {
+var _ = AppsDescribe("Uploading and Downloading droplets", func() {
 	var helloWorldAppName string
 	var out bytes.Buffer
 
@@ -39,7 +43,9 @@ var _ = AppsDescribe("Downloading droplets", func() {
 		Expect(cf.Cf("delete", helloWorldAppName, "-f", "-r").Wait(Config.DefaultTimeoutDuration())).To(Exit(0))
 	})
 
-	It("Downloads the droplet for the app", func() {
+	It("Users can manage droplet bits for an app", func() {
+		By("Downloading the droplet for the app")
+
 		guid := cf.Cf("app", helloWorldAppName, "--guid").Wait(Config.DefaultTimeoutDuration()).Out.Contents()
 		appGuid := strings.TrimSpace(string(guid))
 
@@ -58,5 +64,41 @@ var _ = AppsDescribe("Downloading droplets", func() {
 		Expect(out.String()).To(ContainSubstring("./app/config.ru"))
 		Expect(out.String()).To(ContainSubstring("./tmp"))
 		Expect(out.String()).To(ContainSubstring("./logs"))
+
+		By("Pushing a different version of the app")
+
+		Expect(cf.Cf("push", helloWorldAppName, "-p", assets.NewAssets().Dora).Wait(Config.DefaultTimeoutDuration())).To(Exit(0))
+		Eventually(func() string {
+			return helpers.CurlAppRoot(Config, helloWorldAppName)
+		}, Config.DefaultTimeoutDuration()).Should(ContainSubstring("Hi, I'm Dora!"))
+
+		By("Uploading the originally downloaded droplet of the app")
+
+		token := v3_helpers.GetAuthToken()
+		uploadUrl := fmt.Sprintf("%s%s/v2/apps/%s/droplet/upload", Config.Protocol(), Config.GetApiEndpoint(), appGuid)
+		bits := fmt.Sprintf(`droplet=@%s`, app_droplet_path)
+		curl := helpers.Curl(Config, "-v", uploadUrl, "-X", "PUT", "-F", bits, "-H", fmt.Sprintf("Authorization: %s", token)).Wait(Config.DefaultTimeoutDuration())
+		Expect(curl).To(Exit(0))
+
+		var job struct {
+			Metadata struct {
+				Url string `json:"url"`
+			} `json:"metadata"`
+		}
+		bytes := curl.Out.Contents()
+		json.Unmarshal(bytes, &job)
+		pollingUrl := job.Metadata.Url
+
+		Eventually(func() *Session {
+			return cf.Cf("curl", pollingUrl).Wait(Config.DefaultTimeoutDuration())
+		}, Config.DefaultTimeoutDuration()).Should(Say("finished"))
+
+		By("Running the original droplet for the app")
+
+		cf.Cf("restart", helloWorldAppName).Wait(Config.DefaultTimeoutDuration())
+
+		Eventually(func() string {
+			return helpers.CurlAppRoot(Config, helloWorldAppName)
+		}, Config.DefaultTimeoutDuration()).Should(ContainSubstring("Hello, world!"))
 	})
 })
