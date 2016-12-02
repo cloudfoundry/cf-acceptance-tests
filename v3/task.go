371aa99682e6db67333b62c054abeab680db9b7d
Greg Cobb <gcobb@pivotal.io>
2016-12-01 17:55:23 -0800

    Update task cat to use currently existing CLI commands
    [#134370909]
    Signed-off-by: Utako Ueda <uueda@pivotal.io>

--- v3/task.go
+++ v3/task.go
@@ -3,6 +3,8 @@ package v3
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"strings"
 
 	. "github.com/cloudfoundry/cf-acceptance-tests/cats_suite_helpers"
 
@@ -17,19 +19,49 @@ import (
 	. "github.com/onsi/gomega/gexec"
 )
 
+type Result struct {
+	FailureReason string `json:"failure_reason"`
+}
+
+type Task struct {
+	Guid       string `json:"guid"`
+	Command    string `json:"command"`
+	Name       string `json:"name"`
+	State      string `json:"state"`
+	Result     Result `json:"result"`
+	SequenceId int    `json:"sequence_id"`
+}
+
+type Tasks struct {
+	Resources []Task `json:"resources"`
+}
+
+func getTaskDetails(appName string) []string {
+	listCommand := cf.Cf("tasks", appName).Wait(Config.DefaultTimeoutDuration())
+	Expect(listCommand).To(Exit(0))
+	listOutput := string(listCommand.Out.Contents())
+	lines := strings.Split(listOutput, "\n")
+	return strings.Fields(lines[4])
+}
+
+func getGuid(appGuid string, sequenceId string) string {
+	var tasks Tasks
+	readCommand := cf.Cf("curl", fmt.Sprintf("/v3/apps/%s/tasks", appGuid), "-X", "GET").Wait(Config.DefaultTimeoutDuration())
+	Expect(readCommand).To(Exit(0))
+	err := json.Unmarshal(readCommand.Out.Contents(), &tasks)
+	Expect(err).NotTo(HaveOccurred())
+
+	var task Task
+	for _, task = range tasks.Resources {
+		parsedSequenceId, _ := strconv.Atoi(sequenceId)
+		if parsedSequenceId == task.SequenceId {
+			break
+		}
+	}
+	return task.Guid
+}
+
 var _ = V3Describe("v3 tasks", func() {
-	type Result struct {
-		FailureReason string `json:"failure_reason"`
-	}
-
-	type Task struct {
-		Guid    string `json:"guid"`
-		Command string `json:"command"`
-		Name    string `json:"name"`
-		State   string `json:"state"`
-		Result  Result `json:"result"`
-	}
-
 	var (
 		appName                         string
 		appGuid                         string
@@ -65,68 +97,82 @@ var _ = V3Describe("v3 tasks", func() {
 	Context("tasks lifecycle", func() {
 		It("can successfully create and run a task", func() {
 			By("creating the task")
-			var createOutput Task
-			postBody := `{"command": "echo 0", "name": "mreow"}`
-			createCommand := cf.Cf("curl", fmt.Sprintf("/v3/apps/%s/tasks", appGuid), "-X", "POST", "-d", postBody).Wait(Config.DefaultTimeoutDuration())
+			taskName := "mreow"
+			command := "ls"
+			createCommand := cf.Cf("run-task", appName, command, "--name", taskName).Wait(Config.DefaultTimeoutDuration())
 			Expect(createCommand).To(Exit(0))
-			err := json.Unmarshal(createCommand.Out.Contents(), &createOutput)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(createOutput.Command).To(Equal("echo 0"))
-			Expect(createOutput.Name).To(Equal("mreow"))
-			Expect(createOutput.State).To(Equal("RUNNING"))
+
+			taskDetails := getTaskDetails(appName)
+			sequenceId := taskDetails[0]
+			outputName := taskDetails[1]
+			outputState := taskDetails[2]
+			ouputCommand := taskDetails[len(taskDetails)-1]
+
+			Expect(ouputCommand).To(Equal(command))
+			Expect(outputName).To(Equal(taskName))
+			Expect(outputState).To(Equal("RUNNING"))
+
+			taskGuid := getGuid(appGuid, sequenceId)
 
 			By("TASK_STARTED AppUsageEvent")
 			usageEvents := LastPageUsageEvents(TestSetup)
-			start_event := AppUsageEvent{Entity{State: "TASK_STARTED", ParentAppGuid: appGuid, ParentAppName: appName, TaskGuid: createOutput.Guid}}
+			start_event := AppUsageEvent{Entity{State: "TASK_STARTED", ParentAppGuid: appGuid, ParentAppName: appName, TaskGuid: taskGuid}}
 			Expect(UsageEventsInclude(usageEvents, start_event)).To(BeTrue())
 
 			By("successfully running")
-			var readOutput Task
+
 			Eventually(func() string {
-				readCommand := cf.Cf("curl", fmt.Sprintf("/v3/tasks/%s", createOutput.Guid), "-X", "GET").Wait(Config.DefaultTimeoutDuration())
-				Expect(readCommand).To(Exit(0))
-				err := json.Unmarshal(readCommand.Out.Contents(), &readOutput)
-				Expect(err).NotTo(HaveOccurred())
-				return readOutput.State
+				taskDetails = getTaskDetails(appName)
+				outputName = taskDetails[1]
+				outputState = taskDetails[2]
+				return outputState
 			}, Config.DefaultTimeoutDuration()).Should(Equal("SUCCEEDED"))
+
+			Expect(outputName).To(Equal(taskName))
 
 			By("TASK_STOPPED AppUsageEvent")
 			usageEvents = LastPageUsageEvents(TestSetup)
-			stop_event := AppUsageEvent{Entity{State: "TASK_STOPPED", ParentAppGuid: appGuid, ParentAppName: appName, TaskGuid: createOutput.Guid}}
+			stop_event := AppUsageEvent{Entity{State: "TASK_STOPPED", ParentAppGuid: appGuid, ParentAppName: appName, TaskGuid: taskGuid}}
 			Expect(UsageEventsInclude(usageEvents, stop_event)).To(BeTrue())
 		})
 	})
 
-	Context("When canceling a task", func() {
-		var taskGuid string
+	Context("When cancelling a task", func() {
+		var taskId string
+		var taskName string
 
 		BeforeEach(func() {
-			postBody := `{"command": "sleep 100;", "name": "mreow"}`
-			createCommand := cf.Cf("curl", fmt.Sprintf("/v3/apps/%s/tasks", appGuid), "-X", "POST", "-d", postBody).Wait(Config.DefaultTimeoutDuration())
+			command := "sleep 100;"
+			taskName = "mreow"
+			createCommand := cf.Cf("run-task", appName, command, "--name", taskName).Wait(Config.DefaultTimeoutDuration())
 			Expect(createCommand).To(Exit(0))
 
-			var createOutput Task
-			err := json.Unmarshal(createCommand.Out.Contents(), &createOutput)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(createOutput.Guid).NotTo(Equal(""))
-			taskGuid = createOutput.Guid
+			taskDetails := getTaskDetails(appName)
+			taskId = taskDetails[0]
 		})
 
 		It("should show task is in FAILED state", func() {
-			var failureReason string
-			cancelCommand := cf.Cf("curl", fmt.Sprintf("/v3/tasks/%s/cancel", taskGuid), "-X", "PUT").Wait(Config.DefaultTimeoutDuration())
-			Expect(cancelCommand).To(Exit(0))
+			terminateCommand := cf.Cf("terminate-task", appName, taskId).Wait(Config.DefaultTimeoutDuration())
+			Expect(terminateCommand).To(Exit(0))
 
+			var outputSequenceId, outputName, outputState string
 			Eventually(func() string {
-				readCommand := cf.Cf("curl", fmt.Sprintf("/v3/tasks/%s", taskGuid), "-X", "GET").Wait(Config.DefaultTimeoutDuration())
-				Expect(readCommand).To(Exit(0))
-
-				var readOutput Task
-				err := json.Unmarshal(readCommand.Out.Contents(), &readOutput)
-				Expect(err).NotTo(HaveOccurred())
-				failureReason = readOutput.Result.FailureReason
-				return readOutput.State
+				taskDetails := getTaskDetails(appName)
+				outputSequenceId = taskDetails[0]
+				outputName = taskDetails[1]
+				outputState = taskDetails[2]
+				return outputState
 			}, Config.DefaultTimeoutDuration()).Should(Equal("FAILED"))
+			Expect(outputName).To(Equal(taskName))
+			taskGuid := getGuid(appGuid, outputSequenceId)
+
+			readCommand := cf.Cf("curl", fmt.Sprintf("/v3/tasks/%s", taskGuid), "-X", "GET").Wait(Config.DefaultTimeoutDuration())
+			Expect(readCommand).To(Exit(0))
+
+			var readOutput Task
+			err := json.Unmarshal(readCommand.Out.Contents(), &readOutput)
+			Expect(err).NotTo(HaveOccurred())
+			failureReason := readOutput.Result.FailureReason
 			Expect(failureReason).To(Equal("task was cancelled"))
 		})
 	})
