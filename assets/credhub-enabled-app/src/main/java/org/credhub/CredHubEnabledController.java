2973a662daa53830de19269d8db3e36e14485759
Ben Moss <bmoss@pivotal.io>
2018-02-13 14:49:51 -0500

    Update credhub-enabled-app to latest spring-credhub
    [#155043877] update CATS test fixture `credhub-enabled-app`
    
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- assets/credhub-enabled-app/src/main/java/org/credhub/CredHubEnabledController.java
+++ assets/credhub-enabled-app/src/main/java/org/credhub/CredHubEnabledController.java
@@ -1,38 +1,40 @@
 package org.credhub;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.credhub.core.CredHubTemplate;
-import org.springframework.credhub.support.ServicesData;
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 public class CredHubEnabledController {
-  private CredHubTemplate credHubTemplate;
-  private ServicesData servicesData;
 
-  public CredHubEnabledController(CredHubTemplate credHubTemplate) {
-    this.credHubTemplate = credHubTemplate;
+  private RestTemplate restTemplate;
+  private String interpolateEndpoint;
+  private String serviceOfferingName;
+
+  public CredHubEnabledController(RestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+    this.interpolateEndpoint = System.getenv("CREDHUB_API") + "/api/v1/interpolate";
+    this.serviceOfferingName = System.getenv("SERVICE_NAME") != null ? System.getenv("SERVICE_NAME") : "credhub-read";
   }
 
   @GetMapping({"/test"})
-  public Object runTests() throws Exception {
+  public String runTests() throws Exception {
     String vcapServices = System.getenv("VCAP_SERVICES");
-    String serviceOfferingName = System.getenv("SERVICE_NAME") != null ? System.getenv("SERVICE_NAME") : "credhub-read";
-    return ((Map)((List)this.interpolateServiceData(vcapServices).get(serviceOfferingName)).get(0)).get("credentials");
+    return this.interpolateServiceData(vcapServices).toString();
   }
 
-  private ServicesData interpolateServiceData(String vcapServices) throws IOException {
-    servicesData = this.buildServicesData(vcapServices);
-    return this.credHubTemplate.interpolateServiceData(servicesData);
+  private JSONObject interpolateServiceData(String vcapServices) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    HttpEntity<String> entity = new HttpEntity<>(vcapServices,headers);
+    JSONObject jsonObj = new JSONObject(restTemplate.postForObject(interpolateEndpoint, entity,  String.class));
+
+    return jsonObj.getJSONArray(serviceOfferingName).getJSONObject(0).getJSONObject("credentials");
   }
 
-  private ServicesData buildServicesData(String vcapServices) throws IOException {
-    ObjectMapper mapper = new ObjectMapper();
-    return mapper.readValue(vcapServices, ServicesData.class);
-  }
 }
