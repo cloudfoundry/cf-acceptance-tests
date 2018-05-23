3c885a994c62d97d2d939c1f8e3e220743840480
Yash Tulsiani <ytulsiani@pivotal.io>
2018-05-23 14:59:44 -0700

    Announce app report for failed tests [#157681571]
    Signed-off-by: Iryna Shustava <ishustava@pivotal.io>

--- helpers/app_helpers/app_helpers.go
+++ helpers/app_helpers/app_helpers.go
@@ -4,9 +4,12 @@ import (
 	"strings"
 	"time"
 
+	"fmt"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	. "github.com/cloudfoundry/cf-acceptance-tests/cats_suite_helpers"
 	"github.com/cloudfoundry/cf-acceptance-tests/helpers/logs"
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/config"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gexec"
 )
@@ -24,6 +27,42 @@ func AppReport(appName string, timeout time.Duration) {
 	if appName == "" {
 		return
 	}
+
+	printStartAppReport(appName)
+
 	Eventually(cf.Cf("app", appName, "--guid"), timeout).Should(Exit())
 	Eventually(logs.Tail(Config.GetUseLogCache(), appName), timeout).Should(Exit())
+
+	printEndAppReport(appName)
+}
+
+func printStartAppReport(appName string) {
+	printAppReportBanner(fmt.Sprintf("***** APP REPORT: %s *****", appName))
+}
+
+func printEndAppReport(appName string) {
+	printAppReportBanner(fmt.Sprintf("*** END APP REPORT: %s ***", appName))
+}
+
+func printAppReportBanner(announcement string) {
+	startColor, endColor := getColor()
+	sequence := strings.Repeat("*", len(announcement))
+	fmt.Fprintf(ginkgo.GinkgoWriter,
+		"\n\n%s%s\n%s\n%s%s\n",
+		startColor,
+		sequence,
+		announcement,
+		sequence,
+		endColor)
+}
+
+func getColor() (string, string) {
+	startColor := ""
+	endColor := ""
+	if !config.DefaultReporterConfig.NoColor {
+		startColor = "\x1b[35m"
+		endColor = "\x1b[0m"
+	}
+
+	return startColor, endColor
 }
