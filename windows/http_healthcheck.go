5db65c30851cb1acc4b32f1eb5e798ae651f4896
Andrew Chang <anchang@pivotal.io>
2018-09-04 16:02:15 -0700

    Remove negative windows healthcheck tests [#160212641]
    There's no equivalent test in CATs in the Linux stack, and Diego and
    Windows Garden teams agree that this behavior could be tested just fine
    in a component test
    
    Signed-off-by: Natalie Bennett <nbennett@pivotal.io>

--- windows/http_healthcheck.go
+++ windows/http_healthcheck.go
@@ -42,14 +42,6 @@ var _ = WindowsDescribe("Http Healthcheck", func() {
 	})
 
 	Describe("An app staged and running", func() {
-		It("should not start with an invalid healthcheck endpoint", func() {
-			Expect(cf.Cf("set-health-check", appName, "http", "--endpoint", "/invalidhealthcheck").Wait()).To(Exit(0))
-
-			start := cf.Cf("start", appName)
-			defer start.Kill()
-			Eventually(logs.Out, Config.CfPushTimeoutDuration()*2).Should(Say("health check never passed."))
-		})
-
 		It("starts with a valid http healthcheck endpoint", func() {
 			Expect(cf.Cf("set-health-check", appName, "http", "--endpoint", "/healthcheck").Wait()).To(Exit(0))
 
@@ -64,14 +56,6 @@ var _ = WindowsDescribe("Http Healthcheck", func() {
 			Expect(cf.Cf("start", appName).Wait(Config.CfPushTimeoutDuration())).To(Exit(0))
 
 			Eventually(helpers.CurlingAppRoot(Config, appName)).Should(ContainSubstring("hello i am nora"))
-		})
-
-		It("does not start with a http healthcheck endpoint that is an invalid redirect", func() {
-			Expect(cf.Cf("set-health-check", appName, "http", "--endpoint", "/redirect/invalidhealthcheck").Wait()).To(Exit(0))
-
-			start := cf.Cf("start", appName)
-			defer start.Kill()
-			Eventually(logs.Out, Config.CfPushTimeoutDuration()*2).Should(Say("health check never passed."))
 		})
 	})
 })
