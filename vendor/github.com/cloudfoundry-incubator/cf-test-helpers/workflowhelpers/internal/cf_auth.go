066cc8cecc0f22cf5d3cb94ef60727d52d75c29c
Sebastian Vidrio <svidrio@pivotal.io>
2018-09-07 11:24:09 -0700

    [#160301794] Add verbose logging for auth
    Signed-off-by: Alexander Berezovsky <aberezovsky@pivotal.io>

--- vendor/github.com/cloudfoundry-incubator/cf-test-helpers/workflowhelpers/internal/cf_auth.go
+++ vendor/github.com/cloudfoundry-incubator/cf-test-helpers/workflowhelpers/internal/cf_auth.go
@@ -1,19 +1,28 @@
 package internal
 
 import (
-	"github.com/cloudfoundry-incubator/cf-test-helpers/internal"
-	"github.com/onsi/gomega/gexec"
+	"os"
 	"time"
+
+	"github.com/cloudfoundry-incubator/cf-test-helpers/internal"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
 )
 
-func CfAuth(cmdStarter internal.Starter, reporter internal.Reporter, user string, password string, ) *gexec.Session {
+const VerboseAuth string = "RELINT_VERBOSE_AUTH"
+
+func CfAuth(cmdStarter internal.Starter, reporter internal.Reporter, user string, password string) *gexec.Session {
 	var auth *gexec.Session
 	var err error
 
+	args := []string{"auth", user, password}
+	if os.Getenv(VerboseAuth) == "true" {
+		args = append(args, "-v")
+	}
+
 	retries := 2
 	for i := 1; i <= retries; i++ {
-		auth, err = cmdStarter.Start(reporter, "cf", "auth", user, password)
+		auth, err = cmdStarter.Start(reporter, "cf", args...)
 		if err != nil {
 			panic(err)
 		}
