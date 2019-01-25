cd970492f6b756ee8a88b3e94c3dd48c802f83e8
Peter Goetz <peter.gtz@gmail.com>
2019-01-25 16:18:12 +0100

    Improve error reporting when API redirects multiple times
    'can copy package bits to another app and download the package' fails with a confusing unzip error, when api redirects multiple times, because it tries to unzip the body of a redirect. This change stops on the second redirect with an error message emulating what the cf CLI would do.

--- helpers/download/download.go
+++ helpers/download/download.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"regexp"
+	"strconv"
 
 	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
 	"github.com/cloudfoundry/cf-acceptance-tests/helpers/config"
@@ -22,26 +23,52 @@ func WithRedirect(url, path string, config config.CatsConfig) error {
 		return fmt.Errorf("curl exited with code: %d", downloadCurl.ExitCode())
 	}
 
-	locationHeaderRegex, err := regexp.Compile("(?i)Location: (.*)\r\n")
+	isRedirect, redirectURI, err := CheckRedirect(string(downloadCurl.Err.Contents()))
 	if err != nil {
 		return err
 	}
-
-	matches := locationHeaderRegex.FindStringSubmatch(string(downloadCurl.Err.Contents()))
-	if len(matches) < 2 {
+	if !isRedirect {
 		ioutil.WriteFile(path, downloadCurl.Out.Contents(), 0644)
 		return nil
 	}
 
-	redirectURI := matches[1]
 	downloadCurl = helpers.Curl(
 		config,
 		"-v", redirectURI,
-		"--output", path,
 		"-f",
 	).Wait()
 	if downloadCurl.ExitCode() != 0 {
 		return fmt.Errorf("curl exited with code: %d", downloadCurl.ExitCode())
 	}
-	return nil
+
+	isRedirect, _, err = CheckRedirect(string(downloadCurl.Err.Contents()))
+	if err != nil {
+		return err
+	}
+	if !isRedirect {
+		ioutil.WriteFile(path, downloadCurl.Out.Contents(), 0644)
+		return nil
+	}
+
+	return fmt.Errorf("Only 1 redirect allowed to emulate cf CLI behavior")
+}
+
+func CheckRedirect(curlOutput string) (bool, string, error) {
+	statusCodePattern := `HTTP/\d\.\d (\d{3}) [A-Za-z \-]+`
+	statusCodeMatches := regexp.MustCompile(statusCodePattern).FindStringSubmatch(curlOutput)
+	if len(statusCodeMatches) != 2 {
+		return false, "", fmt.Errorf("Unexpected output from curl. Was expecting %v in the output", statusCodePattern)
+	}
+	statusCode, e := strconv.Atoi(statusCodeMatches[1])
+	if e != nil {
+		return false, "", fmt.Errorf("Unexpected status code from curl: %v", e.Error())
+	}
+	if statusCode > 300 && statusCode < 400 {
+		matches := regexp.MustCompile("(?i)Location: (.*)\r\n").FindStringSubmatch(curlOutput)
+		if len(matches) != 2 {
+			return false, "", fmt.Errorf("Got redirect status code %v, but found no Location in curl output  %v", statusCode, len(matches))
+		}
+		return true, matches[1], nil
+	}
+	return false, "", nil
 }
