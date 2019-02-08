e806118565b7ff7590535281ea41723e3e3d7fc4
Peter Goetz <peter.gtz@gmail.com>
2019-02-08 12:33:12 +0100

    Re-introduce 1200e986bed44040c253eb2fb6c7b4bb10133cd4 and allow HTTP/2 in curl output
    This reverts commit 1200e986bed44040c253eb2fb6c7b4bb10133cd4 and allows HTTP/2 in curl output when downloading bits
    
    More context:
    https://cloudfoundry.slack.com/archives/C0FAEKGUQ/p1549063067139500

---
+++ helpers/download/download_test.go
@@ -0,0 +1,77 @@
+package download_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/cloudfoundry/cf-acceptance-tests/helpers/download"
+)
+
+var _ = Describe("CheckRedirect", func() {
+	Context("Response is a redirect", func() {
+		const moreHeaderInfoIncludingLocation = "< Content-Type: text/html; charset=utf-8\r\n" +
+			"< Date: Fri, 25 Jan 2019 14:51:35 GMT\r\n" +
+			"< Location: https://example.com\r\n" +
+			"< Referrer-Policy: strict-origin-when-cross-origin\r\n" +
+			"< Server: nginx"
+
+		Context("HTTP version 1.1", func() {
+			It("returns true and the redirect location ", func() {
+				isRedirect, location, err := download.CheckRedirect("< HTTP/1.1 302 Found\r\n" + moreHeaderInfoIncludingLocation)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(isRedirect).To(BeTrue())
+				Expect(location).To(Equal("https://example.com"))
+			})
+
+		})
+
+		Context("HTTP version 2", func() {
+			It("returns true and the redirect location ", func() {
+				isRedirect, location, err := download.CheckRedirect("< HTTP/2 302 Found\r\n" + moreHeaderInfoIncludingLocation)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(isRedirect).To(BeTrue())
+				Expect(location).To(Equal("https://example.com"))
+			})
+		})
+
+		Context("Location is missing", func() {
+			It("returns an error", func() {
+				_, _, err := download.CheckRedirect("< HTTP/1.1 302 Found\r\n" +
+					"< Content-Type: text/html; charset=utf-8\r\n" +
+					"< Date: Fri, 25 Jan 2019 14:51:35 GMT\r\n" +
+					"< Referrer-Policy: strict-origin-when-cross-origin\r\n" +
+					"< Server: nginx")
+
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+
+	Context("Response is not a redirect", func() {
+		const moreHeaderInfo = "< Content-Type: text/html; charset=utf-8\r\n" +
+			"< Date: Fri, 25 Jan 2019 14:51:35 GMT\r\n" +
+			"< Server: nginx"
+
+		Context("HTTP version 1.1", func() {
+			It("returns false and empty redirect location ", func() {
+				isRedirect, location, err := download.CheckRedirect("< HTTP/1.1 200 OK\r\n" + moreHeaderInfo)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(isRedirect).To(BeFalse())
+				Expect(location).To(BeEmpty())
+			})
+		})
+
+		Context("HTTP version 2", func() {
+			It("returns false and empty redirect location ", func() {
+				isRedirect, location, err := download.CheckRedirect("< HTTP/2 200 OK\r\n" + moreHeaderInfo)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(isRedirect).To(BeFalse())
+				Expect(location).To(BeEmpty())
+			})
+		})
+	})
+})
