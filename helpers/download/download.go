dea225478410f190ce6fdfe2a508c385055c7a34
Peter Goetz <peter.gtz@gmail.com>
2019-02-20 13:15:19 +0100

    Introduce curl download redirects (3rd attempt)
    This change now allows HTTP/2 and is more lenient on the HTTP line, allowing it without textual status code
    
    More context:
    https://cloudfoundry.slack.com/archives/C0FAEKGUQ/p1550623866022300?thread_ts=1550478410.020200&cid=C0FAEKGUQ
    https://cloudfoundry.slack.com/archives/C0FAEKGUQ/p1549063067139500

--- helpers/download/download.go
+++ helpers/download/download.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"regexp"
+	"strconv"
 
 	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
 	"github.com/cloudfoundry/cf-acceptance-tests/helpers/config"
 	"github.com/cloudfoundry/cf-acceptance-tests/helpers/v3_helpers"
 )
+
+const maxNumRedirects = 10 // This is the same default Go uses with in its http library: https://godoc.org/net/http#Get
 
 func WithRedirect(url, path string, config config.CatsConfig) error {
 	oauthToken := v3_helpers.GetAuthToken()
@@ -22,26 +25,52 @@ func WithRedirect(url, path string, config config.CatsConfig) error {
 		return fmt.Errorf("curl exited with code: %d", downloadCurl.ExitCode())
 	}
 
-	locationHeaderRegex, err := regexp.Compile("(?i)Location: (.*)\r\n")
+	isRedirect, redirectURI, err := CheckRedirect(string(downloadCurl.Err.Contents()))
 	if err != nil {
 		return err
 	}
-
-	matches := locationHeaderRegex.FindStringSubmatch(string(downloadCurl.Err.Contents()))
-	if len(matches) < 2 {
+	if !isRedirect {
 		ioutil.WriteFile(path, downloadCurl.Out.Contents(), 0644)
 		return nil
 	}
+	for i := 0; i < maxNumRedirects; i++ {
+		downloadCurl := helpers.Curl(
+			config,
+			"-v", redirectURI,
+			"-f",
+		).Wait()
+		if downloadCurl.ExitCode() != 0 {
+			return fmt.Errorf("curl exited with code: %d", downloadCurl.ExitCode())
+		}
 
-	redirectURI := matches[1]
-	downloadCurl = helpers.Curl(
-		config,
-		"-v", redirectURI,
-		"--output", path,
-		"-f",
-	).Wait()
-	if downloadCurl.ExitCode() != 0 {
-		return fmt.Errorf("curl exited with code: %d", downloadCurl.ExitCode())
+		isRedirect, redirectURI, err = CheckRedirect(string(downloadCurl.Err.Contents()))
+		if err != nil {
+			return err
+		}
+		if !isRedirect {
+			ioutil.WriteFile(path, downloadCurl.Out.Contents(), 0644)
+			return nil
+		}
 	}
-	return nil
+	return fmt.Errorf("Only %v redirects allowed", maxNumRedirects)
+}
+
+func CheckRedirect(curlOutput string) (bool, string, error) {
+	statusCodePattern := `HTTP/\d(?:\.\d)? (\d{3})[A-Za-z \-]*`
+	statusCodeMatches := regexp.MustCompile(statusCodePattern).FindStringSubmatch(curlOutput)
+	if len(statusCodeMatches) != 2 {
+		return false, "", fmt.Errorf("Unexpected output from curl. Was expecting %v in the following output: %v", statusCodePattern, curlOutput)
+	}
+	statusCode, e := strconv.Atoi(statusCodeMatches[1])
+	if e != nil {
+		return false, "", fmt.Errorf("Unexpected status code from curl: %v", e.Error())
+	}
+	if statusCode > 300 && statusCode < 400 {
+		matches := regexp.MustCompile("(?i)Location: (.*)\r\n").FindStringSubmatch(curlOutput)
+		if len(matches) != 2 {
+			return false, "", fmt.Errorf("Got redirect status code %v, but found no Location in curl output  %v", statusCode, len(matches))
+		}
+		return true, matches[1], nil
+	}
+	return false, "", nil
 }
