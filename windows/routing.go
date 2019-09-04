84d9af4cda547e8ff1fc2c878b26849ba10997be
Andrew Costa <acosta@pivotal.io>
2019-09-04 11:46:18 -0700

    Get next batch of windows tests running on cliv7
    - Removed -d flag from cf push commands
    
    Co-authored-by: Weyman Fung <wfung@pivotal.io>
    Co-authored-by: Dave Walter <dwalter@pivotal.io>

--- windows/routing.go
+++ windows/routing.go
@@ -26,7 +26,7 @@ var _ = WindowsDescribe("Adding and removing routes", func() {
 			"-b", Config.GetHwcBuildpackName(),
 			"-m", DEFAULT_WINDOWS_MEMORY_LIMIT,
 			"-p", assets.NewAssets().Nora,
-			"-d", Config.GetAppsDomain()).Wait(Config.CfPushTimeoutDuration())).To(Exit(0))
+		).Wait(Config.CfPushTimeoutDuration())).To(Exit(0))
 		Eventually(helpers.CurlingAppRoot(Config, appName)).Should(ContainSubstring("hello i am nora"))
 	})
 
