45830fe79646b1c9516e3968e400d9e159347b33
Andrew Costa <acosta@pivotal.io>
2019-09-04 12:03:26 -0700

    Get next batch of windows tests running in cliv7
    - Removed -d flag from cf push commands
    - Pended set start test as app wasn't found
    
    Co-authored-by: Weyman Fung <wfung@pivotal.io>
    Co-authored-by: Dave Walter <dwalter@pivotal.io>

--- windows/standalone_webapp.go
+++ windows/standalone_webapp.go
@@ -34,7 +34,7 @@ var _ = WindowsDescribe("A standalone webapp", func() {
 			"-c", ".\\webapp.exe",
 			"-m", DEFAULT_WINDOWS_MEMORY_LIMIT,
 			"-p", assets.NewAssets().WindowsWebapp,
-			"-d", Config.GetAppsDomain()).Wait(Config.CfPushTimeoutDuration())).To(Exit(0))
+		).Wait(Config.CfPushTimeoutDuration())).To(Exit(0))
 		Eventually(helpers.CurlingAppRoot(Config, appName)).Should(ContainSubstring("hi i am a standalone webapp"))
 	})
 })
