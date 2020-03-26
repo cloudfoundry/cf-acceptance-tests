13c4bccf20144c7c5d867ca7afa638d92370ca8f
Belinda Liu <bliu@pivotal.io>
2020-03-26 11:36:18 -0700

    Add new test for rolling deployments
    This test does a `cf push --strategy=rolling` and continuously curls the
    app to ensure it never goes down.
    
    Adding this test because it is a new feature in the V7 CF CLI, enabled
    by new V3 CAPI features.
    
    [#171547437](https://www.pivotaltracker.com/story/show/171547437)
    
    Co-authored-by: Belinda Liu <bliu@pivotal.io>
    Co-authored-by: Reid Mitchell <rmitchell@pivotal.io>
    Co-authored-by: Weyman Fung <wfung@pivotal.io>

---
+++ apps/rolling_deploy.go
@@ -0,0 +1,73 @@
+package apps
+
+import (
+	"time"
+
+	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
+	. "github.com/cloudfoundry/cf-acceptance-tests/cats_suite_helpers"
+	"github.com/cloudfoundry/cf-acceptance-tests/helpers/app_helpers"
+	"github.com/cloudfoundry/cf-acceptance-tests/helpers/assets"
+	"github.com/cloudfoundry/cf-acceptance-tests/helpers/random_name"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
+)
+
+var _ = AppsDescribe("Rolling deploys", func() {
+	var (
+		appName string
+	)
+
+	BeforeEach(func() {
+		appName = random_name.CATSRandomName("APP")
+
+		Expect(cf.Push(appName,
+			"-p", assets.NewAssets().Dora,
+		).Wait(Config.CfPushTimeoutDuration())).To(Exit(0))
+
+		Eventually(func() string {
+			return helpers.CurlAppRoot(Config, appName)
+		}).Should(ContainSubstring("Hi, I'm Dora!"))
+	})
+
+	AfterEach(func() {
+		app_helpers.AppReport(appName)
+
+		Expect(cf.Cf("delete", appName, "-f", "-r").Wait()).To(Exit(0))
+	})
+
+	It("deploys the app with zero downtime", func() {
+		By("checking the app remains available")
+		doneChannel := make(chan bool, 1)
+		ticker := time.NewTicker(1 * time.Second)
+		tickerChannel := ticker.C
+
+		defer func() {
+			ticker.Stop()
+			close(doneChannel)
+		}()
+
+		go func() {
+			defer GinkgoRecover()
+
+			for {
+				select {
+				case <-doneChannel:
+					ticker.Stop()
+					return
+				case <-tickerChannel:
+					appResponse := helpers.CurlAppRoot(Config, appName)
+					Expect(appResponse).ToNot(ContainSubstring("404 Not Found"))
+					Expect(appResponse).To(ContainSubstring("Hi, I'm Dora!"))
+				}
+			}
+		}()
+
+		Expect(cf.Cf(
+			"push", appName,
+			"-p", assets.NewAssets().Dora,
+			"--strategy=rolling",
+		).Wait(Config.CfPushTimeoutDuration())).To(Exit(0))
+	})
+})
