d83aa7759c9843f32588b247020119a5d5ddf9b4
Eric Promislow <epromislow@suse.com>
2020-07-08 11:14:40 -0700

    Best way to kill this process is to have it exit
    process.Kill() failed to kill the process
    
    [#173293404](https://www.pivotaltracker.com/story/show/173293404)

--- assets/catnip/signal/signal.go
+++ assets/catnip/signal/signal.go
@@ -1,14 +1,10 @@
 package signal
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 )
 
 func KillHandler(res http.ResponseWriter, req *http.Request) {
-	currentProcess, _ := os.FindProcess(os.Getpid())
-	currentProcess.Kill()
-	fmt.Println("killing with os.Exit")
 	os.Exit(1)
 }
