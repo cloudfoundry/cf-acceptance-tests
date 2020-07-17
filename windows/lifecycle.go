1bedc4a2c60e7c73fe6bb4b44476e582f8736559
Eric Promislow <epromislow@suse.com>
2020-07-17 15:34:45 -0700

    For unknown reasons, this test needs an explicit sleep
    Then the framework sees 2 instances running. Otherwise it fails
    when only 1 is running, and doesn't retry.
    
    [#168207994](https://www.pivotaltracker.com/story/show/168207994)

--- windows/lifecycle.go
+++ windows/lifecycle.go
@@ -94,6 +94,7 @@ var _ = WindowsDescribe("Application Lifecycle", func() {
 
 		By("scaling it", func() {
 			Expect(cf.Cf("scale", appName, "-i", "2").Wait(Config.CfPushTimeoutDuration())).To(Exit(0))
+			time.Sleep(5 * time.Second)
 			Eventually(cf.Cf("apps")).Should(gbytes.Say("2/2"))
 			Expect(cf.Cf("app", appName).Wait()).ToNot(gbytes.Say("insufficient resources"))
 		})
