554e66a0e82cf0290cb72224977768e07b7e43cd
Chris Selzo <cselzo@pivotal.io>
2020-08-13 18:21:52 +0000

    routing suit no longer uses v2 endpoiints
    - brought over destination helper function from baras tests
    
    [finishes #173993254]
    
    Co-authored-by: Chris Selzo <cselzo@pivotal.io>
    Co-authored-by: Merric de Launey <mdelauney@pivotal.io>
    Co-authored-by: Chris Selzo <cselzo@pivotal.io>

---
+++ helpers/v3_helpers/destination.go
@@ -0,0 +1,52 @@
+package v3_helpers
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
+)
+
+type DestinationProcess struct {
+	Type string `json:"type"`
+}
+
+type App struct {
+	GUID    string              `json:"guid"`
+	Process *DestinationProcess `json:"process,omitempty"`
+}
+
+type Destination struct {
+	GUID   string `json:"guid,omitempty"`
+	App    App    `json:"app"`
+	Port   int    `json:"port,omitempty"`
+	Weight int    `json:"weight,omitempty"`
+}
+
+type Destinations struct {
+	Destinations []Destination `json:"destinations"`
+}
+
+func InsertDestinations(routeGUID string, destinations []Destination) []string {
+	destinationsJSON, err := json.Marshal(Destinations{Destinations: destinations})
+	Expect(err).ToNot(HaveOccurred())
+
+	session := cf.Cf("curl", "-f",
+		fmt.Sprintf("/v3/routes/%s/destinations", routeGUID),
+		"-X", "POST", "-d", string(destinationsJSON))
+
+	Expect(session.Wait()).To(Exit(0))
+	response := session.Out.Contents()
+
+	var responseDestinations Destinations
+	err = json.Unmarshal(response, &responseDestinations)
+	Expect(err).ToNot(HaveOccurred())
+
+	listDstGUIDs := make([]string, 0, len(responseDestinations.Destinations))
+	for _, dst := range responseDestinations.Destinations {
+		listDstGUIDs = append(listDstGUIDs, dst.GUID)
+	}
+	return listDstGUIDs
+}
