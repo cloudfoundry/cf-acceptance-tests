2732190be027efda8c5e8a98154c2ef47771e178
Seth Boyles <sboyles@pivotal.io>
2022-08-30 17:34:17 +0000

    Unmarshal Route JSON instead of parsing as a string
    Co-authored-by: Seth Boyles <sboyles@pivotal.io>
    Co-authored-by: Merric de Launey <mdelauney@pivotal.io>
    Co-authored-by: David Alvarado <alvaradoda@vmware.com>

--- helpers/v3_helpers/routes.go
+++ helpers/v3_helpers/routes.go
@@ -1,8 +1,8 @@
 package v3_helpers
 
 import (
+	"encoding/json"
 	"fmt"
-	"regexp"
 
 	"github.com/cloudfoundry/cf-test-helpers/v2/cf"
 	. "github.com/onsi/gomega"
@@ -13,7 +13,14 @@ func GetRouteGuid(hostname string) string {
 	routeQuery := fmt.Sprintf("/v3/routes?hosts=%s", hostname)
 	getRoutesCurl := cf.Cf("curl", routeQuery)
 	Expect(getRoutesCurl.Wait()).To(Exit(0))
+	routesJSON := struct {
+		Resources []struct {
+			Guid string `json:"guid"`
+		} `json:"resources"`
+	}{}
+	bytes := getRoutesCurl.Out.Contents()
 
-	routeGuidRegex := regexp.MustCompile(`\s+"guid": "(.+)"`)
-	return routeGuidRegex.FindStringSubmatch(string(getRoutesCurl.Out.Contents()))[1]
+	json.Unmarshal(bytes, &routesJSON)
+
+	return routesJSON.Resources[0].Guid
 }
