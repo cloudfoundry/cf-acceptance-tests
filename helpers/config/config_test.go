d0dc8e9c42f5b4f7299018c2139c7d1f0a0fc726
David Sabeti <sabetid@vmware.com>
2023-07-21 09:19:23 -0700

    Allow all stacks (#891)
    * Allow use of any set of stacks
    
    Given recent efforts to integrate elements of the CNB ecosystem into CF
    -- in particular, [this proposal to integrate Paketo stacks](https://github.com/cloudfoundry/community/pull/637) --
    CATs should allow operators to specify additional stacks besides cflinuxfs4.
    
    * Update README to reflect change to `stacks` config.

--- helpers/config/config_test.go
+++ helpers/config/config_test.go
@@ -711,27 +711,15 @@ var _ = Describe("Config", func() {
 		})
 	})
 
-	Context("when providing invalid stacks property", func() {
+	Context("when providing any set of stacks in the stacks property", func() {
 		BeforeEach(func() {
-			testCfg.Stacks = &[]string{"my-custom-stack"}
-		})
-
-		It("returns error if a stack other cflinuxfs4 is provided", func() {
-			_, err := cfg.NewCatsConfig(tmpFilePath)
-			Expect(err).To(HaveOccurred())
-			Expect(err).To(MatchError("* Invalid configuration: unknown stack 'my-custom-stack'. Only 'cflinuxfs4' is supported for the 'stacks' property"))
-		})
-	})
-
-	Context("when providing valid stacks property", func() {
-		BeforeEach(func() {
-			testCfg.Stacks = &[]string{"cflinuxfs4"}
+			testCfg.Stacks = &[]string{"cflinuxfs4", "my-custom-stack"}
 		})
 
 		It("is loaded into the config", func() {
 			config, err := cfg.NewCatsConfig(tmpFilePath)
 			Expect(err).ToNot(HaveOccurred())
-			Expect(config.GetStacks()).To(Equal([]string{"cflinuxfs4"}))
+			Expect(config.GetStacks()).To(Equal([]string{"cflinuxfs4", "my-custom-stack"}))
 		})
 	})
 
