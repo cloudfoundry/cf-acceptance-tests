a6286f0a970bb0920efaf79cba64d29034fe3f92
Carson Long <lcarson@vmware.com>
2023-09-14 09:31:49 -0700

    fix: application 404 expectations
    In case operators are overriding their router's error pages, we should
    not check for a specific string. However, the presence of "404" on a 404
    error page is probably a safe assumption to make.

--- windows/readiness_healthcheck.go
+++ windows/readiness_healthcheck.go
@@ -70,7 +70,7 @@ var _ = WindowsDescribe("Readiness Healthcheck", func() {
 			By("verifying the app is removed from the routing table")
 			Eventually(func() string {
 				return helpers.CurlApp(Config, appName, "/ready")
-			}, readinessHealthCheckTimeout).Should(ContainSubstring("404 Not Found"))
+			}, readinessHealthCheckTimeout).Should(ContainSubstring("404"))
 
 			By("verifying that the app hasn't restarted")
 			Consistently(cf.Cf("events", appName)).ShouldNot(Say("audit.app.process.crash"))
