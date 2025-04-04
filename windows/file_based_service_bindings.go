aa45d95bc04992678751fdf981a5317f15b78676
serdar özer <serdar.oezer@sap.com>
2025-04-04 19:20:45 +0200

    feat: Incorporate feature flags into file based service bindings tests according to updated RFC-0030 (#1509)
    More details: https://github.com/cloudfoundry/community/blob/main/toc/rfc/rfc-0030-add-support-for-file-based-service-binding.mdq.

--- windows/file_based_service_bindings.go
+++ windows/file_based_service_bindings.go
@@ -2,7 +2,6 @@ package windows
 
 import (
 	"fmt"
-
 	. "github.com/cloudfoundry/cf-acceptance-tests/cats_suite_helpers"
 	"github.com/cloudfoundry/cf-acceptance-tests/helpers/app_helpers"
 	"github.com/cloudfoundry/cf-acceptance-tests/helpers/random_name"
@@ -15,11 +14,53 @@ import (
 )
 
 var _ = FileBasedServiceBindingsDescribe("File Based Service Bindings", WindowsLifecycle, func() {
-	var appName, serviceName string
+	var appName, serviceName, serviceGuid, appGuid, appFeatureFlag string
 
-	BeforeEach(func() {
+	JustBeforeEach(func() {
 		appName = random_name.CATSRandomName("APP")
 		serviceName = generator.PrefixedRandomName("cats", "svin")
+
+		tags := "list, of, tags"
+		creds := `{"username": "admin", "password":"pa55woRD"}`
+		Expect(cf.Cf("create-user-provided-service", serviceName, "-p", creds, "-t", tags).Wait()).To(Exit(0))
+		serviceGuid = services.GetServiceInstanceGuid(serviceName)
+
+		Expect(cf.Cf("create-app", appName).Wait()).To(Exit(0))
+		appGuid = app_helpers.GetAppGuid(appName)
+
+		appFeatureUrl := fmt.Sprintf("/v3/apps/%s/features/%s", appGuid, appFeatureFlag)
+		Expect(cf.Cf("curl", appFeatureUrl, "-X", "PATCH", "-d", `{"enabled": true}`).Wait()).To(Exit(0))
+
+		Expect(cf.Cf("bind-service", appName, serviceName).Wait()).To(Exit(0))
+
+		Expect(cf.Cf(app_helpers.WindowsCatnipWithArgs(
+			appName,
+			"-m", DEFAULT_MEMORY_LIMIT)...,
+		).Wait(Config.CfPushTimeoutDuration())).To(Exit(0))
+
+	})
+
+	Context("When the file-based-vcap-services feature enabled", func() {
+
+		BeforeEach(func() {
+			appFeatureFlag = "file-based-vcap-services"
+		})
+
+		It("It should store the VCAP_SERVICE binding information in file in the VCAP_SERVICES_FILE_PATH", func() {
+			services.ValidateFileBasedVcapServices(appName, serviceName, appGuid, serviceGuid)
+
+		})
+	},
+	)
+
+	Context("When the service-binding-k8s feature enabled", func() {
+
+		BeforeEach(func() {
+			appFeatureFlag = "service-binding-k8s"
+		})
+		It("It should have environment variable SERVICE_BINDING_ROOT which defines the location for the service binding", func() {
+			services.ValidateServiceBindingK8s(appName, serviceName, appGuid, serviceGuid)
+		})
 	})
 
 	AfterEach(func() {
@@ -29,24 +70,4 @@ var _ = FileBasedServiceBindingsDescribe("File Based Service Bindings", WindowsL
 		Eventually(cf.Cf("delete-service", serviceName, "-f").Wait()).Should(Exit(0))
 	})
 
-	It("creates the required files in the app container", func() {
-		tags := "list, of, tags"
-		creds := `{"username": "admin", "password":"pa55woRD"}`
-		Expect(cf.Cf("create-user-provided-service", serviceName, "-p", creds, "-t", tags).Wait()).To(Exit(0))
-		serviceGuid := services.GetServiceInstanceGuid(serviceName)
-
-		Expect(cf.Cf("create-app", appName).Wait()).To(Exit(0))
-		appGuid := app_helpers.GetAppGuid(appName)
-
-		appFeatureUrl := fmt.Sprintf("/v3/apps/%s/features/file-based-service-bindings", appGuid)
-		Expect(cf.Cf("curl", appFeatureUrl, "-X", "PATCH", "-d", `{"enabled": true}`).Wait()).To(Exit(0))
-		Expect(cf.Cf("bind-service", appName, serviceName).Wait()).To(Exit(0))
-
-		Expect(cf.Cf(app_helpers.WindowsCatnipWithArgs(
-			appName,
-			"-m", DEFAULT_MEMORY_LIMIT)...,
-		).Wait(Config.CfPushTimeoutDuration())).To(Exit(0))
-
-		services.ValidateFileBasedServicebinding(appName, serviceName, appGuid, serviceGuid)
-	})
 })
