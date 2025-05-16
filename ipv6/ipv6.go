1b8541c408c490d0a6049a7d2be7314891e6e1df
Milena-Encheva <106135092+Milena-Encheva@users.noreply.github.com>
2025-05-16 08:43:50 +0300

    Update ipv6.go due to copy-paste mistake

--- ipv6/ipv6.go
+++ ipv6/ipv6.go
@@ -81,7 +81,7 @@ var _ = IPv6Describe("IPv6 Connectivity Tests", func() {
 				})
 			})
 
-			Context(fmt.Sprintf("Using Node.js stack: %s", stack), func() {
+			Context(fmt.Sprintf("Using Golang stack: %s", stack), func() {
 				It("validates IPv6 egress for Golang App", func() {
 					describeIPv6Tests(assets.NewAssets().Golang, stack)
 				})
