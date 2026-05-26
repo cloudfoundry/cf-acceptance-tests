be05df45aef1b175c3028cf2be437ed5704959cf
D059372 <johannes.haass@sap.com>
2026-05-26 16:18:08 +0200

    v3: share Dora app setup across process tests via Ordered
    Convert process and process_instances test containers to Ordered, moving
    app push/upload/stage steps from BeforeEach to BeforeAll. Both Its in
    each container are restoration-safe (terminated instances return to
    RUNNING), so sharing setup does not leak state.

--- v3/process.go
+++ v3/process.go
@@ -25,117 +25,119 @@ type ProcessStats struct {
 }
 
 var _ = V3Describe("process", func() {
-	var (
-		appName     string
-		appGuid     string
-		packageGuid string
-		spaceGuid   string
-		token       string
-	)
-
-	BeforeEach(func() {
-		appName = random_name.CATSRandomName("APP")
-		spaceGuid = GetSpaceGuidFromName(TestSetup.RegularUserContext().Space)
-		appGuid = CreateApp(appName, spaceGuid, `{"foo":"bar"}`)
-		packageGuid = CreatePackage(appGuid)
-		token = GetAuthToken()
-		uploadUrl := fmt.Sprintf("%s%s/v3/packages/%s/upload", Config.Protocol(), Config.GetApiEndpoint(), packageGuid)
-		UploadPackage(uploadUrl, assets.NewAssets().DoraZip, token)
-		WaitForPackageToBeReady(packageGuid)
-	})
-
-	AfterEach(func() {
-		app_helpers.AppReport(appName)
-		DeleteApp(appGuid)
-	})
-
-	Describe("terminating an instance", func() {
+	Describe("process scenarios", Ordered, func() {
 		var (
-			index       = 0
-			processType = "web"
-			webProcess  Process
+			appName     string
+			appGuid     string
+			packageGuid string
+			spaceGuid   string
+			token       string
 		)
 
-		BeforeEach(func() {
-			buildGuid := StageBuildpackPackage(packageGuid, Config.GetRubyBuildpackName())
-			WaitForBuildToStage(buildGuid)
-			dropletGuid := GetDropletFromBuild(buildGuid)
-
-			AssignDropletToApp(appGuid, dropletGuid)
-
-			processes := GetProcesses(appGuid, appName)
-			webProcess = GetProcessByType(processes, "web")
-
-			CreateAndMapRoute(appGuid, Config.GetAppsDomain(), webProcess.Name)
-
-			StartApp(appGuid)
-
-			Eventually(func() string {
-				return helpers.CurlAppRoot(Config, webProcess.Name)
-			}, Config.CfPushTimeoutDuration()).Should(ContainSubstring("Hi, I'm Dora!"))
-
-			Expect(string(cf.Cf("apps").Wait().Out.Contents())).To(MatchRegexp(fmt.Sprintf("(v3-)?(%s)*(-web)?(\\s)+(started)", webProcess.Name)))
+		BeforeAll(func() {
+			appName = random_name.CATSRandomName("APP")
+			spaceGuid = GetSpaceGuidFromName(TestSetup.RegularUserContext().Space)
+			appGuid = CreateApp(appName, spaceGuid, `{"foo":"bar"}`)
+			packageGuid = CreatePackage(appGuid)
+			token = GetAuthToken()
+			uploadUrl := fmt.Sprintf("%s%s/v3/packages/%s/upload", Config.Protocol(), Config.GetApiEndpoint(), packageGuid)
+			UploadPackage(uploadUrl, assets.NewAssets().DoraZip, token)
+			WaitForPackageToBeReady(packageGuid)
 		})
 
-		Context("/v3/apps/:guid/processes/:type/instances/:index", func() {
-			It("restarts the instance", func() {
-				statsUrl := fmt.Sprintf("/v3/apps/%s/processes/web/stats", appGuid)
+		AfterAll(func() {
+			app_helpers.AppReport(appName)
+			DeleteApp(appGuid)
+		})
 
-				By("ensuring the instance is running")
-				statsBody := cf.Cf("curl", statsUrl).Wait().Out.Contents()
-				statsJSON := ProcessStats{}
-				json.Unmarshal(statsBody, &statsJSON)
-				Expect(statsJSON.Instance[0].State).To(Equal("RUNNING"))
+		Describe("terminating an instance", func() {
+			var (
+				index       = 0
+				processType = "web"
+				webProcess  Process
+			)
 
-				By("terminating the instance")
-				terminateUrl := fmt.Sprintf("/v3/apps/%s/processes/%s/instances/%d", appGuid, processType, index)
-				cf.Cf("curl", terminateUrl, "-X", "DELETE").Wait()
+			BeforeAll(func() {
+				buildGuid := StageBuildpackPackage(packageGuid, Config.GetRubyBuildpackName())
+				WaitForBuildToStage(buildGuid)
+				dropletGuid := GetDropletFromBuild(buildGuid)
 
-				By("ensuring the instance is no longer running")
-				// Note that this depends on a 30s run loop waking up in Diego.
+				AssignDropletToApp(appGuid, dropletGuid)
+
+				processes := GetProcesses(appGuid, appName)
+				webProcess = GetProcessByType(processes, "web")
+
+				CreateAndMapRoute(appGuid, Config.GetAppsDomain(), webProcess.Name)
+
+				StartApp(appGuid)
+
 				Eventually(func() string {
-					statsBodyAfter := cf.Cf("curl", statsUrl).Wait().Out.Contents()
-					json.Unmarshal(statsBodyAfter, &statsJSON)
-					return statsJSON.Instance[0].State
-				}, V3_PROCESS_TIMEOUT, 1*time.Second).ShouldNot(Equal("RUNNING"))
+					return helpers.CurlAppRoot(Config, webProcess.Name)
+				}, Config.CfPushTimeoutDuration()).Should(ContainSubstring("Hi, I'm Dora!"))
 
-				By("ensuring the instance is running again")
-				Eventually(func() string {
-					statsBodyAfter := cf.Cf("curl", statsUrl).Wait().Out.Contents()
-					json.Unmarshal(statsBodyAfter, &statsJSON)
-					return statsJSON.Instance[0].State
-				}, Config.CfPushTimeoutDuration(), 1*time.Second).Should(Equal("RUNNING"))
+				Expect(string(cf.Cf("apps").Wait().Out.Contents())).To(MatchRegexp(fmt.Sprintf("(v3-)?(%s)*(-web)?(\\s)+(started)", webProcess.Name)))
 			})
-		})
 
-		Context("/v3/processes/:guid/instances/:index", func() {
-			It("restarts the instance", func() {
-				statsUrl := fmt.Sprintf("/v3/apps/%s/processes/web/stats", appGuid)
+			Context("/v3/apps/:guid/processes/:type/instances/:index", func() {
+				It("restarts the instance", func() {
+					statsUrl := fmt.Sprintf("/v3/apps/%s/processes/web/stats", appGuid)
 
-				By("ensuring the instance is running")
-				statsBody := cf.Cf("curl", statsUrl).Wait().Out.Contents()
-				statsJSON := ProcessStats{}
-				json.Unmarshal(statsBody, &statsJSON)
-				Expect(statsJSON.Instance[0].State).To(Equal("RUNNING"))
+					By("ensuring the instance is running")
+					statsBody := cf.Cf("curl", statsUrl).Wait().Out.Contents()
+					statsJSON := ProcessStats{}
+					json.Unmarshal(statsBody, &statsJSON)
+					Expect(statsJSON.Instance[0].State).To(Equal("RUNNING"))
 
-				By("terminating the instance")
-				terminateUrl := fmt.Sprintf("/v3/processes/%s/instances/%d", webProcess.Guid, index)
-				cf.Cf("curl", terminateUrl, "-X", "DELETE").Wait()
+					By("terminating the instance")
+					terminateUrl := fmt.Sprintf("/v3/apps/%s/processes/%s/instances/%d", appGuid, processType, index)
+					cf.Cf("curl", terminateUrl, "-X", "DELETE").Wait()
 
-				By("ensuring the instance is no longer running")
-				// Note that this depends on a 30s run loop waking up in Diego.
-				Eventually(func() string {
-					statsBodyAfter := cf.Cf("curl", statsUrl).Wait().Out.Contents()
-					json.Unmarshal(statsBodyAfter, &statsJSON)
-					return statsJSON.Instance[0].State
-				}, V3_PROCESS_TIMEOUT, 1*time.Second).ShouldNot(Equal("RUNNING"))
+					By("ensuring the instance is no longer running")
+					// Note that this depends on a 30s run loop waking up in Diego.
+					Eventually(func() string {
+						statsBodyAfter := cf.Cf("curl", statsUrl).Wait().Out.Contents()
+						json.Unmarshal(statsBodyAfter, &statsJSON)
+						return statsJSON.Instance[0].State
+					}, V3_PROCESS_TIMEOUT, 1*time.Second).ShouldNot(Equal("RUNNING"))
 
-				By("ensuring the instance is running again")
-				Eventually(func() string {
-					statsBodyAfter := cf.Cf("curl", statsUrl).Wait().Out.Contents()
-					json.Unmarshal(statsBodyAfter, &statsJSON)
-					return statsJSON.Instance[0].State
-				}, Config.CfPushTimeoutDuration(), 1*time.Second).Should(Equal("RUNNING"))
+					By("ensuring the instance is running again")
+					Eventually(func() string {
+						statsBodyAfter := cf.Cf("curl", statsUrl).Wait().Out.Contents()
+						json.Unmarshal(statsBodyAfter, &statsJSON)
+						return statsJSON.Instance[0].State
+					}, Config.CfPushTimeoutDuration(), 1*time.Second).Should(Equal("RUNNING"))
+				})
+			})
+
+			Context("/v3/processes/:guid/instances/:index", func() {
+				It("restarts the instance", func() {
+					statsUrl := fmt.Sprintf("/v3/apps/%s/processes/web/stats", appGuid)
+
+					By("ensuring the instance is running")
+					statsBody := cf.Cf("curl", statsUrl).Wait().Out.Contents()
+					statsJSON := ProcessStats{}
+					json.Unmarshal(statsBody, &statsJSON)
+					Expect(statsJSON.Instance[0].State).To(Equal("RUNNING"))
+
+					By("terminating the instance")
+					terminateUrl := fmt.Sprintf("/v3/processes/%s/instances/%d", webProcess.Guid, index)
+					cf.Cf("curl", terminateUrl, "-X", "DELETE").Wait()
+
+					By("ensuring the instance is no longer running")
+					// Note that this depends on a 30s run loop waking up in Diego.
+					Eventually(func() string {
+						statsBodyAfter := cf.Cf("curl", statsUrl).Wait().Out.Contents()
+						json.Unmarshal(statsBodyAfter, &statsJSON)
+						return statsJSON.Instance[0].State
+					}, V3_PROCESS_TIMEOUT, 1*time.Second).ShouldNot(Equal("RUNNING"))
+
+					By("ensuring the instance is running again")
+					Eventually(func() string {
+						statsBodyAfter := cf.Cf("curl", statsUrl).Wait().Out.Contents()
+						json.Unmarshal(statsBodyAfter, &statsJSON)
+						return statsJSON.Instance[0].State
+					}, Config.CfPushTimeoutDuration(), 1*time.Second).Should(Equal("RUNNING"))
+				})
 			})
 		})
 	})
--- v3/process_instances.go
+++ v3/process_instances.go
@@ -36,37 +36,74 @@ type ProcessesResource struct {
 }
 
 var _ = V3Describe("process instances", func() {
-	var (
-		appName    string
-		webProcess v3_helpers.Process
-	)
+	Describe("process instance scenarios", Ordered, func() {
+		var (
+			appName    string
+			webProcess v3_helpers.Process
+		)
 
-	BeforeEach(func() {
-		appName = random_name.CATSRandomName("APP")
+		BeforeAll(func() {
+			appName = random_name.CATSRandomName("APP")
 
-		By("pushing the app with three instances")
-		Expect(cf.Cf("push", appName, "-i", "3", "-b", Config.GetRubyBuildpackName(), "-p", assets.NewAssets().DoraZip).Wait(Config.CfPushTimeoutDuration())).To(Exit(0))
+			By("pushing the app with three instances")
+			Expect(cf.Cf("push", appName, "-i", "3", "-b", Config.GetRubyBuildpackName(), "-p", assets.NewAssets().DoraZip).Wait(Config.CfPushTimeoutDuration())).To(Exit(0))
 
-		By("waiting until all instances are running")
-		Eventually(func(g Gomega) {
-			session := cf.Cf("app", appName).Wait()
-			g.Expect(session).Should(Say(`instances:\s+3/3`))
-		}).Should(Succeed())
+			By("waiting until all instances are running")
+			Eventually(func(g Gomega) {
+				session := cf.Cf("app", appName).Wait()
+				g.Expect(session).Should(Say(`instances:\s+3/3`))
+			}).Should(Succeed())
 
-		appGuid := app_helpers.GetAppGuid(appName)
-		processes := v3_helpers.GetProcesses(appGuid, appName)
-		webProcess = v3_helpers.GetProcessByType(processes, "web")
-	})
+			appGuid := app_helpers.GetAppGuid(appName)
+			processes := v3_helpers.GetProcesses(appGuid, appName)
+			webProcess = v3_helpers.GetProcessByType(processes, "web")
+		})
 
-	AfterEach(func() {
-		app_helpers.AppReport(appName)
-		Expect(cf.Cf("delete", appName, "-f").Wait(Config.DefaultTimeoutDuration())).To(Exit(0))
-	})
+		AfterAll(func() {
+			app_helpers.AppReport(appName)
+			Expect(cf.Cf("delete", appName, "-f").Wait(Config.DefaultTimeoutDuration())).To(Exit(0))
+		})
 
-	Context("via different v3 API endpoints", func() {
-		It("returns an array of process instances", func() {
-			By("/v3/processes/:guid/process_instances")
-			{
+		Context("via different v3 API endpoints", func() {
+			It("returns an array of process instances", func() {
+				By("/v3/processes/:guid/process_instances")
+				{
+					resource := ProcessInstancesResource{}
+					getResource(fmt.Sprintf("v3/processes/%s/process_instances", webProcess.Guid), &resource)
+					Expect(len(resource.ProcessInstances)).To(Equal(3))
+
+					for index, instance := range resource.ProcessInstances {
+						checkProcessInstance(instance, index, "RUNNING")
+					}
+				}
+
+				By("/v3/processes/:guid?embed=process_instances")
+				{
+					resource := ProcessResource{}
+					getResource(fmt.Sprintf("v3/processes/%s?embed=process_instances", webProcess.Guid), &resource)
+					Expect(len(resource.ProcessInstances)).To(Equal(3))
+
+					for index, instance := range resource.ProcessInstances {
+						checkProcessInstance(instance, index, "RUNNING")
+					}
+				}
+
+				By("/v3/processes?guids=:guid&embed=process_instances")
+				{
+					resource := ProcessesResource{}
+					getResource(fmt.Sprintf("v3/processes?guids=%s&embed=process_instances", webProcess.Guid), &resource)
+					Expect(len(resource.Processes)).To(Equal(1))
+					Expect(len(resource.Processes[0].ProcessInstances)).To(Equal(3))
+
+					for index, instance := range resource.Processes[0].ProcessInstances {
+						checkProcessInstance(instance, index, "RUNNING")
+					}
+				}
+			})
+		})
+
+		Context("when stopping the app", func() {
+			It("switches the instances' state from RUNNING to DOWN", func() {
 				resource := ProcessInstancesResource{}
 				getResource(fmt.Sprintf("v3/processes/%s/process_instances", webProcess.Guid), &resource)
 				Expect(len(resource.ProcessInstances)).To(Equal(3))
@@ -74,60 +111,25 @@ var _ = V3Describe("process instances", func() {
 				for index, instance := range resource.ProcessInstances {
 					checkProcessInstance(instance, index, "RUNNING")
 				}
-			}
 
-			By("/v3/processes/:guid?embed=process_instances")
-			{
-				resource := ProcessResource{}
-				getResource(fmt.Sprintf("v3/processes/%s?embed=process_instances", webProcess.Guid), &resource)
+				By("stopping the app")
+				Expect(cf.Cf("stop", appName).Wait(Config.DefaultTimeoutDuration())).To(Exit(0))
+
+				By("waiting until all instances are stopped")
+				Eventually(func() []string {
+					getResource(fmt.Sprintf("v3/processes/%s/process_instances", webProcess.Guid), &resource)
+					states := []string{}
+					for _, instance := range resource.ProcessInstances {
+						states = append(states, instance.State)
+					}
+					return states
+				}, V3_PROCESS_TIMEOUT, 1*time.Second).Should(Equal([]string{"DOWN", "DOWN", "DOWN"}))
 				Expect(len(resource.ProcessInstances)).To(Equal(3))
 
 				for index, instance := range resource.ProcessInstances {
-					checkProcessInstance(instance, index, "RUNNING")
+					checkProcessInstance(instance, index, "DOWN")
 				}
-			}
-
-			By("/v3/processes?guids=:guid&embed=process_instances")
-			{
-				resource := ProcessesResource{}
-				getResource(fmt.Sprintf("v3/processes?guids=%s&embed=process_instances", webProcess.Guid), &resource)
-				Expect(len(resource.Processes)).To(Equal(1))
-				Expect(len(resource.Processes[0].ProcessInstances)).To(Equal(3))
-
-				for index, instance := range resource.Processes[0].ProcessInstances {
-					checkProcessInstance(instance, index, "RUNNING")
-				}
-			}
-		})
-	})
-
-	Context("when stopping the app", func() {
-		It("switches the instances' state from RUNNING to DOWN", func() {
-			resource := ProcessInstancesResource{}
-			getResource(fmt.Sprintf("v3/processes/%s/process_instances", webProcess.Guid), &resource)
-			Expect(len(resource.ProcessInstances)).To(Equal(3))
-
-			for index, instance := range resource.ProcessInstances {
-				checkProcessInstance(instance, index, "RUNNING")
-			}
-
-			By("stopping the app")
-			Expect(cf.Cf("stop", appName).Wait(Config.DefaultTimeoutDuration())).To(Exit(0))
-
-			By("waiting until all instances are stopped")
-			Eventually(func() []string {
-				getResource(fmt.Sprintf("v3/processes/%s/process_instances", webProcess.Guid), &resource)
-				states := []string{}
-				for _, instance := range resource.ProcessInstances {
-					states = append(states, instance.State)
-				}
-				return states
-			}, V3_PROCESS_TIMEOUT, 1*time.Second).Should(Equal([]string{"DOWN", "DOWN", "DOWN"}))
-			Expect(len(resource.ProcessInstances)).To(Equal(3))
-
-			for index, instance := range resource.ProcessInstances {
-				checkProcessInstance(instance, index, "DOWN")
-			}
+			})
 		})
 	})
 })
